668240253a8819b25f7cdc6cf762cc97b92e7bfc
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-02-17 16:05:16 +0100

    Add support for .env files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,3 @@
+require('dotenv').config()
+
 import './shared/infrastructure/rest/app'
--- backend/src/shared/infrastructure/rest/app.ts
+++ backend/src/shared/infrastructure/rest/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.REST_API_PORT || 5000;
 
 app.listen(port, () => {
   console.log(`[App]: Listening on port ${port}`)
