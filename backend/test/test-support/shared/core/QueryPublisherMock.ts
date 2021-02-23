fc2835cbcaaa2a72b14194471f37d5ac6d71e2f8
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-02-23 16:17:46 +0100

    Ts config build

--- backend/test/test-support/shared/core/QueryPublisherMock.ts
+++ backend/test/test-support/shared/core/QueryPublisherMock.ts
@@ -1,7 +1,7 @@
 import { QueryPublisher } from '../../../../src/shared/core/application/query/QueryBus';
 import { Query } from '../../../../src/shared/core/application/query/Query';
 
-export function QueryPublisherMock(alwaysReturn: any): QueryPublisher & { executeCalls: jest.Mock } {
+export function QueryPublisherMock(alwaysReturn: any = undefined): QueryPublisher & { executeCalls: jest.Mock } {
   const executeCalls: jest.Mock = jest.fn();
   return {
     async execute<ResultType = any, QueryType extends Query = Query>(query: QueryType): Promise<ResultType> {
