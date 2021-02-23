f0b44d92035569103e9fa1a18f804c31bd4d9ce4
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-02-23 11:07:50 +0100

    Add QueryHandler comment hint

--- backend/src/modules/tournaments-registrations/core/application/query/FindAllTournamentRegistrationsQueryHandler.ts
+++ backend/src/modules/tournaments-registrations/core/application/query/FindAllTournamentRegistrationsQueryHandler.ts
@@ -3,6 +3,12 @@ import { QueryHandler } from '../../../../../shared/core/application/query/Query
 import { FindTournamentRegistrationsById } from './FindTournamentRegistrationsById';
 import { FindAllTournamentRegistrations, FindAllTournamentRegistrationsResult } from './FindAllTournamentRegistrations';
 
+/**
+ * HINT
+ * This style of implementation is using domain model.
+ * But in this case, changes in domain model will be reflected in query result.
+ * Another option, which is preferred by me, it's creating read model by handling events.
+ */
 export class FindAllTournamentRegistrationsQueryHandler
   implements QueryHandler<FindAllTournamentRegistrations, FindAllTournamentRegistrationsResult> {
   constructor(private readonly repository: TournamentRegistrationsRepository) {}
