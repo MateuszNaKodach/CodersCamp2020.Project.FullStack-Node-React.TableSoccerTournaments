d83fff7568fb4a090330bc09ace002701b502489
Tomek <tomasz.dworniczak@hotmail.com>
2021-02-28 00:26:26 +0100

    #5 Fix bug in query handler

--- backend/src/modules/doubles-tournament/core/application/query/FindAllDoublesTournamentsQueryHandler.ts
+++ backend/src/modules/doubles-tournament/core/application/query/FindAllDoublesTournamentsQueryHandler.ts
@@ -1,12 +1,12 @@
 import { QueryHandler } from '../../../../../shared/core/application/query/QueryHandler';
 import { FindAllDoublesTournaments, FindAllDoublesTournamentsResult } from './FindAllDoublesTournaments';
 import { DoublesTournamentRepository } from '../DoublesTournamentRepository';
-import { FindDoublesTournamentByIdResult } from './FindDoublesTournamentById';
+import { FindDoublesTournamentById } from './FindDoublesTournamentById';
 
 export class FindAllDoublesTournamentsQueryHandler implements QueryHandler<FindAllDoublesTournaments, FindAllDoublesTournamentsResult> {
   constructor(private readonly repository: DoublesTournamentRepository) {}
 
-  execute(query: FindDoublesTournamentByIdResult): Promise<FindAllDoublesTournamentsResult> {
+  execute(query: FindDoublesTournamentById): Promise<FindAllDoublesTournamentsResult> {
     return this.repository.findAll();
   }
 }
