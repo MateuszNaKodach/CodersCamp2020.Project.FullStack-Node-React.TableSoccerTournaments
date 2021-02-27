3fe4e0eddac2fbd574ea2a78ef5df34d6f2c3052
Tomek <tomasz.dworniczak@hotmail.com>
2021-02-28 00:16:11 +0100

    #5 Code format

--- backend/src/modules/doubles-tournament/core/domain/DoublesTournament.ts
+++ backend/src/modules/doubles-tournament/core/domain/DoublesTournament.ts
@@ -1,32 +1,54 @@
 import {TournamentTeam} from './TournamentTeam';
 import {TournamentWithTeamsWasCreated} from './event/TournamentWithTeamsWasCreated';
-import {DomainEvent} from '../../../../shared/domain/event/DomainEvent';
 import {EntityIdGenerator} from '../../../../shared/core/application/EntityIdGenerator';
+import {DomainCommandResult} from '../../../../shared/core/domain/DomainCommandResult';
 
 export class DoublesTournament {
-  readonly tournamentId: string;
-  readonly tournamentTeams: TournamentTeam[];
+    readonly tournamentId: string;
+    readonly tournamentTeams: TournamentTeam[];
 
-  constructor(props: { tournamentId: string; tournamentTeams: TournamentTeam[] }) {
-    this.tournamentId = props.tournamentId;
-    this.tournamentTeams = props.tournamentTeams;
-  }
+    constructor(props: { tournamentId: string; tournamentTeams: TournamentTeam[] }) {
+        this.tournamentId = props.tournamentId;
+        this.tournamentTeams = props.tournamentTeams;
+    }
 }
 
 export function createTournamentWithTeams(
-  command: { tournamentId: string; tournamentPairs: { player1: string; player2: string }[] },
-  currentTime: Date,
-  entityIdGenerator: EntityIdGenerator,
-): { events: DomainEvent[] } {
-  const tournamentTeams: TournamentTeam[] = command.tournamentPairs.map((tournamentPair) => {
-    const teamId = entityIdGenerator.generate();
-    return new TournamentTeam({
-      teamId: teamId,
-      firstTeamPlayer: tournamentPair.player1,
-      secondTeamPlayer: tournamentPair.player2,
+    state: DoublesTournament | undefined,
+    command: { tournamentId: string; tournamentPairs: { player1: string; player2: string }[] },
+    currentTime: Date,
+    entityIdGenerator: EntityIdGenerator,
+): DomainCommandResult<DoublesTournament> {
+    if (state !== undefined) {
+        throw new Error('This tournament already exists.');
+    }
+
+    const tournamentTeams: TournamentTeam[] = command.tournamentPairs.map((tournamentPair) => {
+        const teamId = entityIdGenerator.generate();
+        return new TournamentTeam({
+            teamId: teamId,
+            firstTeamPlayer: tournamentPair.player1,
+            secondTeamPlayer: tournamentPair.player2,
+        });
     });
-  });
-  return {
-    events: [new TournamentWithTeamsWasCreated({ occurredAt: currentTime, tournamentId: command.tournamentId, tournamentTeams })],
-  };
+
+    const tournamentWithTeamsWasCreated = new TournamentWithTeamsWasCreated({
+        occurredAt: currentTime,
+        tournamentId: command.tournamentId,
+        tournamentTeams,
+    });
+
+    const createdTournamentWithTeams = onTournamentWithTeamsWasCreated(state, tournamentWithTeamsWasCreated);
+
+    return {
+        state: createdTournamentWithTeams,
+        events: [tournamentWithTeamsWasCreated],
+    };
+}
+
+function onTournamentWithTeamsWasCreated(state: DoublesTournament | undefined, event: TournamentWithTeamsWasCreated): DoublesTournament {
+    return new DoublesTournament({
+        tournamentId: event.tournamentId,
+        tournamentTeams: event.tournamentTeams,
+    });
 }
