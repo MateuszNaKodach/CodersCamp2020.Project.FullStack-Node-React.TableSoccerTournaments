9772a90da4493fd915999c60e57880d0d1bea5e1
Pawel Szambelan <p.szambelan1@gmail.com>
2021-02-28 00:04:25 +0100

    #6 test for command create player profile

---
+++ backend/src/modules/tournaments-registrations/infrastructure/repository/inmemory/InMemoryPlayerProfileRepository.ts
@@ -0,0 +1,18 @@
+import {PlayerProfilesRepository} from "../../../../player-profiles/core/application/PlayerProfilesRepository";
+import {PlayerProfile} from "../../../../player-profiles/core/domain/PlayerProfile";
+
+export class InMemoryPlayerProfileRepository implements PlayerProfilesRepository {
+    private readonly entities: { [ id: string]: PlayerProfile } = {};
+
+    findByPlayerId(playerId: string): Promise<PlayerProfile | undefined> {
+        return Promise.resolve(this.entities[playerId]);
+    }
+
+    async save(playerProfile: PlayerProfile): Promise<void> {
+        this.entities[playerProfile.playerId] = playerProfile;
+    }
+
+    findAll(): Promise<PlayerProfile[]> {
+        return Promise.resolve(Object.keys(this.entities).map((id) => this.entities[id]));
+    }
+}
