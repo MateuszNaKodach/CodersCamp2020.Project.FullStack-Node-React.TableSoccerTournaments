9dd9a7ca2f06de8306e2ec0c833fdab54bbd56f1
Pawel Szambelan <p.szambelan1@gmail.com>
2021-03-01 00:07:08 +0100

    #7 make playerId as ObjectValue more changes

--- backend/src/modules/player-profiles/core/application/PlayerProfilesRepository.ts
+++ backend/src/modules/player-profiles/core/application/PlayerProfilesRepository.ts
@@ -1,9 +1,10 @@
 import { PlayerProfile } from '../domain/PlayerProfile';
+import { PlayerId } from '../../../../shared/core/domain/PlayerId';
 
 export interface PlayerProfilesRepository {
   save(playerProfile: PlayerProfile): Promise<void>;
 
-  findByPlayerId(playerId: string): Promise<PlayerProfile | undefined>;
+  findByPlayerId(playerId: PlayerId): Promise<PlayerProfile | undefined>;
 
   findAll(): Promise<PlayerProfile[]>;
 }
--- backend/src/modules/player-profiles/core/application/command/CreatePlayerProfile.ts
+++ backend/src/modules/player-profiles/core/application/command/CreatePlayerProfile.ts
@@ -1,11 +1,13 @@
+import { PlayerId } from '../../../../../shared/core/domain/PlayerId';
+
 export class CreatePlayerProfile {
-  readonly playerId: string;
+  readonly playerId: PlayerId;
   readonly firstName: string;
   readonly lastName: string;
   readonly phoneNumber: string;
   readonly emailAddress: string;
 
-  constructor(props: { playerId: string; firstName: string; lastName: string; emailAddress: string; phoneNumber: string }) {
+  constructor(props: { playerId: PlayerId; firstName: string; lastName: string; emailAddress: string; phoneNumber: string }) {
     this.playerId = props.playerId;
     this.firstName = props.firstName;
     this.lastName = props.lastName;
