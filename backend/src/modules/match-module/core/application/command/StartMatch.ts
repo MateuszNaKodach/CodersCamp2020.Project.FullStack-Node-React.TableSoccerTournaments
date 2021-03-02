c0d9ee6d9a5a7f05affa0c32d5e25bc458881ba8
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-02 23:51:34 +0100

    #26 Create start match command

---
+++ backend/src/modules/match-module/core/application/command/StartMatch.ts
@@ -0,0 +1,12 @@
+export class StartMatch {
+    readonly matchId: string;
+    readonly team1: string;
+    readonly team2: string;
+
+
+    constructor(matchId: string, team1: string, team2: string) {
+        this.matchId = matchId;
+        this.team1 = team1;
+        this.team2 = team2;
+    }
+}
