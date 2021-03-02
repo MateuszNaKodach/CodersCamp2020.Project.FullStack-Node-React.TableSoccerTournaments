9b4864428c33cf5a18baf9a7b2856b6b4d3b1b85
PiotrWR <pwrynio@gmail.com>
2021-03-03 00:28:16 +0100

    Delete unused var

--- backend/src/modules/elimination-tournament-tree/core/domain/TournamentTeam.ts
+++ backend/src/modules/elimination-tournament-tree/core/domain/TournamentTeam.ts
@@ -3,7 +3,7 @@ import { TournamentTeamId } from './TournamentTeamId';
 export class TournamentTeam {
     readonly teamId: TournamentTeamId;
 
-    constructor(props: { teamId: TournamentTeamId; firstTeamPlayer: string; secondTeamPlayer: string }) {
+    constructor(props: { teamId: TournamentTeamId;  }) {
         this.teamId = props.teamId;
     }
 }
