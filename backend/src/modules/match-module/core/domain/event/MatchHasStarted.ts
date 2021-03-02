89f1a1d952b979f9befcb86a74ce14b8413ce61d
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-02 23:52:12 +0100

    #26 Create domain event

---
+++ backend/src/modules/match-module/core/domain/event/MatchHasStarted.ts
@@ -0,0 +1,14 @@
+export class MatchHasStarted {
+    occurredAt: Date;
+    matchId: string;
+    team1: string;
+    team2: string;
+
+
+    constructor(props: { occurredAt: Date, matchId: string, team1: string, team2: string }) {
+        this.occurredAt = props.occurredAt;
+        this.matchId = props.matchId;
+        this.team1 = props.team1;
+        this.team2 = props.team2;
+    }
+}
