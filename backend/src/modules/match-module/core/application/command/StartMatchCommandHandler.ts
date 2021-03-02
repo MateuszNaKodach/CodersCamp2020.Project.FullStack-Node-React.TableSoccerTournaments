df6521aef29cbc7a2b3565c6fe791147bce4e0f7
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-03 00:22:08 +0100

    #26 Add command handler

---
+++ backend/src/modules/match-module/core/application/command/StartMatchCommandHandler.ts
@@ -0,0 +1,20 @@
+import {CommandHandler} from "../../../../../shared/core/application/command/CommandHandler";
+import {StartMatch} from "./StartMatch";
+import {DomainEventPublisher} from "../../../../../shared/core/application/event/DomainEventBus";
+import {CurrentTimeProvider} from "../../../../../shared/core/CurrentTimeProvider";
+import {CommandResult} from "../../../../../shared/core/application/command/CommandResult";
+import {startMatch} from "../../domain/Match";
+
+export class StartMatchCommandHandler implements CommandHandler<StartMatch> {
+    constructor(
+        private readonly eventPublisher: DomainEventPublisher,
+        private readonly currentTimeProvider: CurrentTimeProvider,
+    ) {}
+
+    async execute(command: StartMatch): Promise<CommandResult> {
+
+        const { events } = startMatch(command, this.currentTimeProvider());
+        this.eventPublisher.publishAll(events);
+        return CommandResult.success();
+    }
+}
