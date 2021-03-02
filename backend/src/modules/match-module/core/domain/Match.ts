5541ddf8c87ffce89e62f72bc46f9dc7e04f64a5
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-03 00:23:48 +0100

    #26 Create domain object (draft without types)

---
+++ backend/src/modules/match-module/core/domain/Match.ts
@@ -0,0 +1,23 @@
+import {DomainEvent} from "../../../../shared/domain/event/DomainEvent";
+import {MatchHasStarted} from "./event/MatchHasStarted";
+
+export class Match {
+    readonly matchId: string;
+    readonly team1: string;
+    readonly team2: string;
+
+    constructor(props: { matchId: string, team1: string, team2: string }) {
+        this.matchId = props.matchId;
+        this.team1 = props.team1;
+        this.team2 = props.team2;
+    }
+}
+
+export function startMatch(
+    command: { matchId: string; team1: string, team2: string },
+    currentTime: Date,
+): { events: DomainEvent[] } {
+    return {
+        events: [new MatchHasStarted({ occurredAt: currentTime, matchId: command.matchId, team1: command.team1, team2: command.team2 })]
+    }
+}
