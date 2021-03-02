677511caec1fd712c3e6b2e31a25e60666dffc30
PiotrWR <pwrynio@gmail.com>
2021-03-02 21:55:54 +0100

    Add empty Core.ts

---
+++ backend/src/modules/elimination-tournament-tree/core/EliminationTournamentTreeCore.ts
@@ -0,0 +1,17 @@
+import {ModuleCore} from "../../../shared/core/ModuleCore";
+
+export function DoublesTournamentModuleCore(
+    // eventPublisher: DomainEventPublisher,
+    // commandPublisher: CommandPublisher,
+    // currentTimeProvider: CurrentTimeProvider,
+    // entityIdGenerator: EntityIdGenerator,
+    // repository: DoublesTournamentRepository,
+): ModuleCore {
+
+    return {
+        commandHandlers: [],
+        eventHandlers: [],
+        queryHandlers: [],
+    };
+}
+
