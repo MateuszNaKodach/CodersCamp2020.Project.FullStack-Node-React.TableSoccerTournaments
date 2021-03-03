dc4f44de14e6ecb8f5fcb2c4e61e7b01e2cdba37
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-03 18:57:44 +0100

    #26 Register query handlers in module core

--- backend/src/modules/match-module/core/MatchModuleCore.ts
+++ backend/src/modules/match-module/core/MatchModuleCore.ts
@@ -4,20 +4,35 @@ import { CurrentTimeProvider } from '../../../shared/core/CurrentTimeProvider';
 import { ModuleCore } from '../../../shared/core/ModuleCore';
 import { StartMatch } from './application/command/StartMatch';
 import { StartMatchCommandHandler } from './application/command/StartMatchCommandHandler';
+import {FindMatchById} from "./application/query/FindMatchById";
+import {FindMatchByIdQueryHandler} from "./application/query/FindMatchByIdQueryHandler";
+import {MatchRepository} from "./application/MatchRepository";
+import {FindAllMatches} from "./application/query/FindAllMatches";
+import {FindAllMatchesQueryHandler} from "./application/query/FindAllMatchesQueryHandler";
 
 export function MatchModuleCore(
   eventPublisher: DomainEventPublisher,
   commandPublisher: CommandPublisher,
   currentTimeProvider: CurrentTimeProvider,
+  repository: MatchRepository
 ): ModuleCore {
   return {
     commandHandlers: [
       {
         commandType: StartMatch,
-        handler: new StartMatchCommandHandler(eventPublisher, currentTimeProvider),
+        handler: new StartMatchCommandHandler(eventPublisher, currentTimeProvider, repository),
       },
     ],
     eventHandlers: [],
-    queryHandlers: [],
+    queryHandlers: [
+      {
+        queryType: FindMatchById,
+        handler: new FindMatchByIdQueryHandler(repository),
+      },
+      {
+        queryType: FindAllMatches,
+        handler: new FindAllMatchesQueryHandler(repository),
+      },
+    ],
   };
 }
