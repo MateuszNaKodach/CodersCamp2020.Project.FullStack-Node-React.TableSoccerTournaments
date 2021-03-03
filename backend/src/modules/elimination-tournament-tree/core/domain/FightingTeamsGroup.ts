333cd0a3a06d915be1ea1337f9a04b06e25ded33
PiotrWR <pwrynio@gmail.com>
2021-03-03 01:16:02 +0100

    Add teams to FightingTeamsGroup

--- backend/src/modules/elimination-tournament-tree/core/domain/FightingTeamsGroup.ts
+++ backend/src/modules/elimination-tournament-tree/core/domain/FightingTeamsGroup.ts
@@ -1,9 +1,16 @@
 import { FightingTeamsGroupId } from './FightingTeamsGroupId';
+import {TournamentTeam} from './TournamentTeam';
 
 export class FightingTeamsGroup {
     readonly fightingTeamsGroupId: FightingTeamsGroupId;
+    firstTeam :TournamentTeam;
+    secondTeam :TournamentTeam;
 
-    constructor(props: { fightingTeamsGroupId: FightingTeamsGroupId }) {
+
+    constructor(props: { fightingTeamsGroupId: FightingTeamsGroupId , firstTeam: TournamentTeam, secondTeam : TournamentTeam}) {
         this.fightingTeamsGroupId = props.fightingTeamsGroupId;
+        this.firstTeam = props.firstTeam;
+        this.secondTeam = props.secondTeam;
+
     }
 }
