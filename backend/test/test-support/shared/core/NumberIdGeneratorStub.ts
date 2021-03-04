e1b8edf43a89dc3874f771be8f41225c0ba3c570
PiotrWR <pwrynio@gmail.com>
2021-03-04 23:31:13 +0100

    Add NumberIdGeneratorStub

---
+++ backend/test/test-support/shared/core/NumberIdGeneratorStub.ts
@@ -0,0 +1,13 @@
+import { EntityIdGenerator } from '../../../../src/shared/core/application/EntityIdGenerator';
+
+export function NumberIdGeneratorStub(numberOfIds: number = 1, idsName: string = ""): EntityIdGenerator {
+  const idsList = Array.from(Array(numberOfIds)
+      .keys())
+      .map(numberItem => `${idsName}${++numberItem}`)
+      .reverse();
+  return {
+    generate(): string {
+      return <string>idsList.pop();
+    },
+  };
+}
