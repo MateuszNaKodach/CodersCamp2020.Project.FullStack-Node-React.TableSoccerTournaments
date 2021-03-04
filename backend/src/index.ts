c4edd9f7e66c6f3c8fed4647beb27f39c38b0ec7
Pawel Szambelan <p.szambelan1@gmail.com>
2021-03-04 18:53:19 +0100

    #32 try to test nodemailer

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,37 @@
 import { TableSoccerTournamentsApplication } from './app';
 
 const port = process.env.REST_API_PORT || 5000;
+const nodemailer = require("nodemailer");
+
 
 TableSoccerTournamentsApplication().then(({ restApi }) =>
   restApi.listen(port, () => {
     console.log(`[App]: REST API listening on port ${port}`);
     console.log(`[App]: You can access REST API documentation at http://localhost:${port}/rest-api-docs`);
+
+      const transporter = nodemailer.createTransport({
+          host: "mailhog",
+          port: 1025
+      });
+
+      restApi.get("/send_email/:email", (req, res, next) => {
+          try {
+          const { email } = req.params;
+
+          const messageStatus = transporter.sendMail({
+              from: "My Company <company@companydomain.org>",
+              to: email,
+              subject: "Hi Mailhog!",
+              text: "This is the email content",
+          });
+
+          if (!messageStatus) res.json("Error sending message!").status(500);
+
+          res.json("Sent!").status(200);
+          } catch (error) {
+              res.status(error.response.status)
+              return res.send(error.message);
+          }
+      });
   }),
 );
