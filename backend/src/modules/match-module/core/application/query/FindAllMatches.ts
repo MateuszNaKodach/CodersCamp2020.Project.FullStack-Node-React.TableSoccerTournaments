3911c752eb873dc39be71819219f99bfe9286ccf
Tomek <50457289+tomdworniczak@users.noreply.github.com>
2021-03-04 12:25:34 +0100

    Match module/application and domain layer (#30)

---
+++ backend/src/modules/match-module/core/application/query/FindAllMatches.ts
@@ -0,0 +1,7 @@
+import { Match } from '../../domain/Match';
+
+export class FindAllMatches {
+  constructor() {}
+}
+
+export type FindAllMatchesResult = Match[];
---
+++ backend/src/modules/match-module/core/domain/Match.ts
@@ -0,0 +1,63 @@
+import { MatchHasStarted } from './event/MatchHasStarted';
+import { MatchId } from './MatchId';
+import { MatchSideId } from './MatchSideId';
+import { DomainCommandResult } from '../../../../shared/core/domain/DomainCommandResult';
+
+export class Match {
+  readonly matchId: MatchId;
+  readonly firstMatchSideId: MatchSideId | undefined;
+  readonly secondMatchSideId: MatchSideId | undefined;
+  readonly winner: MatchSideId | undefined;
+  readonly looser: MatchSideId | undefined;
+  readonly hasEnded: boolean | undefined = false;
+
+  constructor(props: {
+    matchId: MatchId;
+    firstMatchSideId: MatchSideId;
+    secondMatchSideId: MatchSideId;
+    winner?: MatchSideId;
+    looser?: MatchSideId;
+    hasEnded?: boolean;
+  }) {
+    this.matchId = props.matchId;
+    this.firstMatchSideId = props.firstMatchSideId;
+    this.secondMatchSideId = props.secondMatchSideId;
+    this.winner = props.winner;
+    this.looser = props.looser;
+    this.hasEnded = props.hasEnded;
+  }
+}
+
+export function startMatch(
+  state: Match | undefined,
+  command: { matchId: MatchId; firstMatchSideId: MatchSideId; secondMatchSideId: MatchSideId },
+  currentTime: Date,
+): DomainCommandResult<Match> {
+  if (state?.matchId !== undefined) {
+    throw new Error('Cannot start a match that has already begun.');
+  }
+  if (command.firstMatchSideId.equals(command.secondMatchSideId)) {
+    throw new Error('Cannot start match if opposite teams are the same team.');
+  }
+
+  const matchHasStarted = new MatchHasStarted({
+    occurredAt: currentTime,
+    matchId: command.matchId.raw,
+    firstMatchSideId: command.firstMatchSideId.raw,
+    secondMatchSideId: command.secondMatchSideId.raw,
+  });
+  const startedMatch = onMatchHasStarted(state, matchHasStarted);
+
+  return {
+    state: startedMatch,
+    events: [matchHasStarted],
+  };
+}
+
+function onMatchHasStarted(state: Match | undefined, event: MatchHasStarted): Match {
+  return new Match({
+    matchId: MatchId.from(event.matchId),
+    firstMatchSideId: MatchSideId.from(event.firstMatchSideId),
+    secondMatchSideId: MatchSideId.from(event.secondMatchSideId),
+  });
+}
