b1c34c36e58c5abdafe5d9e72358cf8bcadc9f48
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-05 16:01:15 +0100

    #35 Implement match module rest api and router

---
+++ backend/src/modules/match-module/presentation/rest-api/MatchRestApiModule.ts
@@ -0,0 +1,16 @@
+import {CommandPublisher} from "../../../../shared/core/application/command/CommandBus";
+import {DomainEventPublisher} from "../../../../shared/core/application/event/DomainEventBus";
+import {QueryPublisher} from "../../../../shared/core/application/query/QueryBus";
+import {ModuleRestApi} from "../../../../shared/presentation/rest-api/ModuleRestApi";
+import {matchRouter} from "./MatchRouter";
+
+export function MatchRestApiModule(
+    commandPublisher: CommandPublisher,
+    eventPublisher: DomainEventPublisher,
+    queryPublisher: QueryPublisher,
+): ModuleRestApi {
+    return {
+        router: matchRouter(commandPublisher, eventPublisher, queryPublisher),
+        path: '/matches',
+    }
+}
---
+++ backend/src/modules/match-module/presentation/rest-api/MatchRouter.ts
@@ -0,0 +1,26 @@
+import {CommandPublisher} from "../../../../shared/core/application/command/CommandBus";
+import {DomainEventPublisher} from "../../../../shared/core/application/event/DomainEventBus";
+import {QueryPublisher} from "../../../../shared/core/application/query/QueryBus";
+import express, {Request, Response} from 'express';
+import {PostMatchBody} from "./request/PostMatchBody";
+import {StartMatch} from "../../core/application/command/StartMatch";
+import {StatusCodes} from "http-status-codes";
+
+export function matchRouter(
+    commandPublisher: CommandPublisher,
+    eventPublisher: DomainEventPublisher,
+    queryPublisher: QueryPublisher,
+): express.Router {
+    const postStartMatch = async (request: Request, response: Response) => {
+        const requestBody: PostMatchBody = request.body;
+        const commandResult = await commandPublisher.execute(new StartMatch({ matchId: requestBody.matchId, firstMatchSideId: requestBody.firstMatchSideId, secondMatchSideId: requestBody.secondMatchSideId }));
+        return commandResult.process(
+            () => response.status(StatusCodes.CREATED).json({ matchId: requestBody.matchId, firstMatchSideId: requestBody.firstMatchSideId, secondMatchSideId: requestBody.secondMatchSideId }).send(),
+            (failureReason) => response.status(StatusCodes.BAD_REQUEST).json({ message: failureReason.message }),
+        );
+    }
+
+    const router = express.Router();
+    router.post('', postStartMatch);
+    return router;
+}
