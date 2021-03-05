5b805f63d85b423e7a71daadf612807e6db7ccee
PiotrWR <pwrynio@gmail.com>
2021-03-05 13:22:28 +0100

    Not working | during work

---
+++ backend/test/modules/elimination-tournament-tree/core/domain/TouramentTeamsListGenerator.ts
@@ -0,0 +1,8 @@
+import {TournamentTeam} from "../../../../../src/modules/elimination-tournament-tree/core/domain/TournamentTeam";
+import {TournamentTeamId} from "../../../../../src/modules/elimination-tournament-tree/core/domain/TournamentTeamId";
+import {EntityIdGenerator} from "../../../../../src/shared/core/application/EntityIdGenerator";
+
+export function TournamentTeamsListGenerator(entityIdGen: EntityIdGenerator, numberOfTeams = 1): TournamentTeam[] {
+    return Array.from(Array(numberOfTeams))
+        .map(() => new TournamentTeam({teamId: TournamentTeamId.from(entityIdGen.generate())}));
+}
