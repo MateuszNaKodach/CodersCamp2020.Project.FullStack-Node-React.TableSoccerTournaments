2a83f24b1e1ca81775d9941da6348699c5c40d4d
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-03-06 17:53:13 +0100

    Revert "not working fom Mateusz"
    This reverts commit 5dfe0e45

--- backend/src/modules/elimination-tournament-tree/core/domain/TournamentTree.ts
+++ backend/src/modules/elimination-tournament-tree/core/domain/TournamentTree.ts
@@ -2,10 +2,7 @@ import {TournamentTeam} from "./TournamentTeam";
 import {EntityIdGenerator} from "../../../../shared/core/application/EntityIdGenerator";
 import {FightingTeamsGroup} from "./FightingTeamsGroup";
 import {FightingTeamsGroupId} from "./FightingTeamsGroupId";
-import { Status } from 'brackets-model';
-
 import {toUnicode} from "punycode";
-import {BracketsManager} from "brackets-manager";
 
 export class TournamentTree {
     readonly tournamentTeams: TournamentTeam[];
@@ -23,11 +20,6 @@ export function createTournamentTree(
         entityIdGenerator: EntityIdGenerator,
     }
 ): FightingTeamsGroup[] {
-
-    const manager = new BracketsManager(storage);
-
-
-
 
     const numberOfFightingTeamsGroups = fightingTeamsGroupsNumber(props.tournamentTeams.length);
     const winnerTree = createWinnerTree(props.tournamentTeams, numberOfFightingTeamsGroups, props.entityIdGenerator);
