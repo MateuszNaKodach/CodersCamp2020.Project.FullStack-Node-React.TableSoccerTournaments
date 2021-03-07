2e57ee9c0cf1d1a8cde2727c76830f7bc7faaf09
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-07 23:08:41 +0100

    #43 Add id suffix to winner and looser fields

--- backend/src/modules/match-module/core/domain/event/MatchHasEnded.ts
+++ backend/src/modules/match-module/core/domain/event/MatchHasEnded.ts
@@ -1,13 +1,13 @@
 export class MatchHasEnded {
   readonly occurredAt: Date;
   readonly matchId: string;
-  readonly winner: string;
-  readonly looser: string | undefined;
+  readonly winnerId: string;
+  readonly looserId: string;
 
-  constructor(props: { occurredAt: Date; matchId: string; winner: string; looser: string | undefined }) {
+  constructor(props: { occurredAt: Date; matchId: string; winnerId: string; looserId: string }) {
     this.occurredAt = props.occurredAt;
     this.matchId = props.matchId;
-    this.winner = props.winner;
-    this.looser = props.looser;
+    this.winnerId = props.winnerId;
+    this.looserId = props.looserId;
   }
 }
