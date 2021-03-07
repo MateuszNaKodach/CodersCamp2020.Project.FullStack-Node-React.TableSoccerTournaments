209ab162e081c7b1c5477f1610fe746e8165a2ba
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-07 16:26:17 +0100

    #41 Add match has ended event

--- backend/src/modules/match-module/core/domain/event/MatchHasEnded.ts
+++ backend/src/modules/match-module/core/domain/event/MatchHasEnded.ts
@@ -1,3 +1,14 @@
 export class MatchHasEnded {
+    readonly occurredAt: Date;
+    readonly matchId: string;
+    readonly winner: string;
+    readonly looser: string | undefined;
 
+
+    constructor(props: { occurredAt: Date, matchId: string, winner: string, looser: string | undefined }) {
+        this.occurredAt = props.occurredAt;
+        this.matchId = props.matchId;
+        this.winner = props.winner;
+        this.looser = props.looser;
+    }
 }
