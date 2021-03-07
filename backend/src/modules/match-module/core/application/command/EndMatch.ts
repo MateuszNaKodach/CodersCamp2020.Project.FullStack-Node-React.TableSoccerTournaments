e5cb9d395a4b1a62ecb1d6edb32c178efc907019
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-07 16:25:54 +0100

    #41 Add end match command

--- backend/src/modules/match-module/core/application/command/EndMatch.ts
+++ backend/src/modules/match-module/core/application/command/EndMatch.ts
@@ -1,1 +1,10 @@
-export class EndMatch {}
+export class EndMatch {
+    readonly matchId: string;
+    readonly winner: string;
+
+
+    constructor(props: {matchId: string, winner: string }) {
+        this.matchId = props.matchId;
+        this.winner = props.winner;
+    }
+}
