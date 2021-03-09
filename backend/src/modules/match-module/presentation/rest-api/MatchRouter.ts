8644b5c36c3a1f6883e6c3518f1cd1bcbe031a16
Tomek <50457289+tomdworniczak@users.noreply.github.com>
2021-03-09 11:44:57 +0100

    Match Module | POST End match (#47)

--- backend/src/modules/match-module/presentation/rest-api/MatchRouter.ts
+++ backend/src/modules/match-module/presentation/rest-api/MatchRouter.ts
@@ -2,12 +2,14 @@ import { CommandPublisher } from '../../../../shared/core/application/command/Co
 import { DomainEventPublisher } from '../../../../shared/core/application/event/DomainEventBus';
 import { QueryPublisher } from '../../../../shared/core/application/query/QueryBus';
 import express, { Request, Response } from 'express';
-import { PostMatchBody } from './request/PostMatchBody';
+import { PostStartMatchRequestBody } from './request/PostStartMatchRequestBody';
 import { StartMatch } from '../../core/application/command/StartMatch';
 import { StatusCodes } from 'http-status-codes';
 import { FindMatchById, FindMatchByIdResult } from '../../core/application/query/FindMatchById';
 import { Match } from '../../core/domain/Match';
 import { MatchDto } from './response/MatchDto';
+import { PostEndMatchRequestBody } from './request/PostEndMatchRequestBody';
+import { EndMatch } from '../../core/application/command/EndMatch';
 
 export function matchRouter(
   commandPublisher: CommandPublisher,
@@ -15,7 +17,7 @@ export function matchRouter(
   queryPublisher: QueryPublisher,
 ): express.Router {
   const postStartMatch = async (request: Request, response: Response) => {
-    const requestBody: PostMatchBody = request.body;
+    const requestBody: PostStartMatchRequestBody = request.body;
     const commandResult = await commandPublisher.execute(
       new StartMatch({
         matchId: requestBody.matchId,
@@ -29,10 +31,23 @@ export function matchRouter(
           .status(StatusCodes.CREATED)
           .json({
             matchId: requestBody.matchId,
-            firstMatchSideId: requestBody.firstMatchSideId,
-            secondMatchSideId: requestBody.secondMatchSideId,
           })
           .send(),
+      (failureReason) => response.status(StatusCodes.BAD_REQUEST).json({ message: failureReason.message }),
+    );
+  };
+
+  const postEndMatch = async (request: Request, response: Response) => {
+    const { matchId } = request.params;
+    const requestBody: PostEndMatchRequestBody = request.body;
+    const commandResult = await commandPublisher.execute(
+      new EndMatch({
+        matchId: matchId,
+        winnerId: requestBody.winnerId,
+      }),
+    );
+    return commandResult.process(
+      () => response.status(StatusCodes.OK).send(),
       (failureReason) => response.status(StatusCodes.BAD_REQUEST).json({ message: failureReason.message }),
     );
   };
@@ -48,6 +63,7 @@ export function matchRouter(
 
   const router = express.Router();
   router.post('', postStartMatch);
+  router.post('/:matchId/result', postEndMatch);
   router.get('/:matchId', getMatchById);
   return router;
 }
@@ -57,5 +73,6 @@ function toMatchDto(match: Match): MatchDto {
     matchId: match.matchId.raw,
     firstMatchSideId: match.firstMatchSideId.raw,
     secondMatchSideId: match.secondMatchSideId.raw,
+    winnerId: match.winner?.raw,
   });
 }
---
+++ backend/src/modules/match-module/presentation/rest-api/request/PostEndMatchRequestBody.ts
@@ -0,0 +1,3 @@
+export class PostEndMatchRequestBody {
+  readonly winnerId: string;
+}
