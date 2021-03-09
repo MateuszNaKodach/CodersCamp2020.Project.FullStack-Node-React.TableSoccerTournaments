f4dc0bb1601a3ed4634eabd5b2fa119bb1324cf2
Pawel Szambelan <p.szambelan1@gmail.com>
2021-03-09 23:22:06 +0100

    #32 try to write test execute send email command

---
+++ backend/test/modules/player-profile/core/application/SendEmailAfterProfileWasCreated.spec.ts
@@ -0,0 +1,32 @@
+import { testPlayerProfileModule } from './TestPlayerProfileModule';
+import { PlayerProfileWasCreated } from '../../../../../src/modules/player-profiles/core/domain/event/PlayerProfileWasCreated';
+import {CommandPublisherMock} from "../../../../test-support/shared/core/CommandPublisherMock";
+
+describe('Send an email to player', () => {
+  it('When player profile is created, then execute command for email sending', async () => {
+    //Given
+    const currentTime = new Date();
+    const playerProfileModuleCore = testPlayerProfileModule(currentTime);
+    const firstName = 'Johnny';
+    const emailAddress = 'bravo@gmail.com';
+
+    const playerProfileWasCreated = new PlayerProfileWasCreated({
+      occurredAt: currentTime,
+      playerId: 'PlayerId',
+      firstName: firstName,
+      lastName: 'Bravo',
+      emailAddress: emailAddress,
+      phoneNumber: '123456789',
+    });
+
+    const commandPublisher = CommandPublisherMock(); // ?????
+
+    //When
+    playerProfileModuleCore.publishEvent(playerProfileWasCreated);
+
+    //Then
+    expect(commandPublisher.executeCalls()); //????
+
+
+  });
+});
