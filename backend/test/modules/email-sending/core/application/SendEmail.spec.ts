acf8b58f1fc6f1fa043bdfa518ea915b74e3ef87
Pawel Szambelan <p.szambelan1@gmail.com>
2021-03-10 15:37:09 +0100

    #32 fix for test for sending an email in email module

--- backend/test/modules/email-sending/core/application/SendEmail.spec.ts
+++ backend/test/modules/email-sending/core/application/SendEmail.spec.ts
@@ -1,10 +1,16 @@
 import { testSendEmailModule } from './TestSendEmailModule';
 import { SendEmail } from '../../../../../src/modules/email-sending/core/application/command/SendEmail';
+import { EmailSender } from '../../../../../src/modules/email-sending/core/application/EmailSender';
 
 describe('Send an email to player', () => {
   it('When command SendEmail is sent, then send an email with appropriate arguments', async () => {
     //Given
-    const sendEmailModuleCore = testSendEmailModule();
+    const sendEmailFrom = 'TourDeFoos <TourDeFoos@gmail.com>';
+    let emailSender: EmailSender;
+    emailSender = {
+      sendAnEmail: jest.fn(),
+    };
+    const sendEmailModuleCore = testSendEmailModule(emailSender);
     const sendEmail = new SendEmail({
       subject: 'test',
       emailAddress: 'test@gmail.com',
@@ -16,5 +22,12 @@ describe('Send an email to player', () => {
 
     //Then
     expect(commandResult.isSuccess()).toBeTruthy();
+    expect(emailSender.sendAnEmail).toHaveBeenCalled();
+    expect(emailSender.sendAnEmail).toHaveBeenCalledWith({
+      from: sendEmailFrom,
+      to: 'test@gmail.com',
+      subject: 'test',
+      html: '<div>test</div>',
+    });
   });
 });
--- backend/test/modules/email-sending/core/application/TestSendEmailModule.ts
+++ backend/test/modules/email-sending/core/application/TestSendEmailModule.ts
@@ -1,8 +1,7 @@
 import { testModuleCore, TestModuleCore } from '../../../../test-support/shared/core/TestModuleCore';
-import { NodeMailerEmailSender } from '../../../../../src/modules/email-sending/infrastructure/mailer/NodeMailerEmailSender';
 import { SendEmailModuleCore } from '../../../../../src/modules/email-sending/core/SendEmailModuleCore';
+import { EmailSender } from '../../../../../src/modules/email-sending/core/application/EmailSender';
 
-export function testSendEmailModule(): TestModuleCore {
-  const emailSender = new NodeMailerEmailSender({ host: 'localhost', port: 1025, secure: false });
-  return testModuleCore((commandBus, eventBus, queryBus) => SendEmailModuleCore(emailSender));
+export function testSendEmailModule(emailSender: EmailSender): TestModuleCore {
+  return testModuleCore(() => SendEmailModuleCore(emailSender));
 }
