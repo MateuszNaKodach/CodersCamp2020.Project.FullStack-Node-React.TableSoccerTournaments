17da06592298dd6c6d2ab51d48a89835800d2b38
Pawel Szambelan <p.szambelan1@gmail.com>
2021-03-10 16:52:58 +0100

    #32 add mock for commandPublisher

--- backend/test/modules/player-profile/core/application/SendEmailAfterProfileWasCreated.spec.ts
+++ backend/test/modules/player-profile/core/application/SendEmailAfterProfileWasCreated.spec.ts
@@ -1,5 +1,7 @@
 import { testPlayerProfileModule } from './TestPlayerProfileModule';
 import { PlayerProfileWasCreated } from '../../../../../src/modules/player-profiles/core/domain/event/PlayerProfileWasCreated';
+import { CommandPublisher } from '../../../../../src/shared/core/application/command/CommandBus';
+import { SendEmailAfterPlayerProfileWasCreatedEventHandler } from '../../../../../src/modules/player-profiles/core/application/event/SendEmailAfterPlayerProfileWasCreatedEventHandler';
 
 describe('Send an email to player', () => {
   it('When player profile is created, then execute command for email sending', () => {
@@ -18,11 +20,18 @@ describe('Send an email to player', () => {
       phoneNumber: '123456789',
     });
 
+    const commandPublisher: CommandPublisher = {
+      execute: jest.fn(),
+    };
+    new SendEmailAfterPlayerProfileWasCreatedEventHandler(commandPublisher);
+    // const sendEmailAfterPlayerProfileWasCreatedEventHandler = new SendEmailAfterPlayerProfileWasCreatedEventHandler(commandPublisher);
+    // await sendEmailAfterPlayerProfileWasCreatedEventHandler.handle(playerProfileWasCreated);
+
     //When
     playerProfileModuleCore.publishEvent(playerProfileWasCreated);
 
     //Then
-    //what to check here ???
-    // if eventHandler handle event above or if ?commandPublisher? execute command from email module??
+    expect(commandPublisher.execute).toHaveBeenCalled();
+    // expect(sendEmailAfterPlayerProfileWasCreatedEventHandler.handle).toHaveBeenCalled();
   });
 });
