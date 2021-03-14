cbd369a2997498c69d6654d20236c8a2c4165316
Pawel Szambelan <p.szambelan1@gmail.com>
2021-03-15 00:14:48 +0100

    #49 write query and write side tests

---
+++ backend/test/modules/doubles-tournament/core/application/TestDoublesTournamentModuleWithTournament.ts
@@ -0,0 +1,16 @@
+import { TestModuleCore } from '../../../../test-support/shared/core/TestModuleCore';
+import { FromListIdGeneratorStub } from '../../../../test-support/shared/core/FromListIdGeneratorStub';
+import { testDoublesTournamentsModule } from './TestDoublesTournamentsModule';
+import { CreateTournamentWithTeams } from '../../../../../src/modules/doubles-tournament/core/application/command/CreateTournamentWithTeams';
+
+export function createTournamentForTests(tournamentId: string, currentTime: Date, team1Id: string, team2Id: string): TestModuleCore {
+  const entityIdGen = FromListIdGeneratorStub([team1Id, team2Id]);
+  const doublesTournament = testDoublesTournamentsModule(currentTime, entityIdGen);
+  const tournamentPairs = [
+    { player1: 'player1', player2: 'player2' },
+    { player1: 'player3', player2: 'player4' },
+  ];
+  const createTournamentWithTeams = new CreateTournamentWithTeams(tournamentId, tournamentPairs);
+  doublesTournament.executeCommand(createTournamentWithTeams);
+  return doublesTournament;
+}
