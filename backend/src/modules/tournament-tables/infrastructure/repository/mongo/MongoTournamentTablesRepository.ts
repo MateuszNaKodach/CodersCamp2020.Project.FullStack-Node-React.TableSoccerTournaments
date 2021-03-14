86fcb41baf2c2be3f91ecfa68b3b5f933e7944b0
lamparina <anna.lamperska@gmail.com>
2021-03-14 17:08:55 +0100

    Reformat code

--- backend/src/modules/tournament-tables/infrastructure/repository/mongo/MongoTournamentTablesRepository.ts
+++ backend/src/modules/tournament-tables/infrastructure/repository/mongo/MongoTournamentTablesRepository.ts
@@ -4,14 +4,13 @@ import { TournamentTable } from '../../../core/domain/TournamentTable';
 import { TableNumber } from '../../../core/domain/TableNumber';
 
 export class MongoTournamentTablesRepository implements TournamentTablesRepository {
-
   async save(tournamentTable: TournamentTable): Promise<void> {
     await MongoTournamentTables.findByIdAndUpdate(
       { _id: `${tournamentTable.tournamentId}_${tournamentTable.tableNumber.raw}` },
       {
         tournamentId: tournamentTable.tournamentId,
         tableNumber: tournamentTable.tableNumber.raw,
-        tableName: tournamentTable.tableName
+        tableName: tournamentTable.tableName,
       },
       { upsert: true, useFindAndModify: true },
     );
@@ -22,7 +21,7 @@ export class MongoTournamentTablesRepository implements TournamentTablesReposito
   }
 
   async findAllByTournamentId(tournamentId: string): Promise<TournamentTable[]> {
-    const mongoFindResult = await MongoTournamentTables.find({tournamentId});
+    const mongoFindResult = await MongoTournamentTables.find({ tournamentId });
     return mongoDocumentToDomain(mongoFindResult);
   }
 }
@@ -40,17 +39,20 @@ const TournamentTablesSchema = new mongoose.Schema({
   tableNumber: {
     type: Schema.Types.Number,
     min: 1,
-    max: 200
+    max: 200,
   },
-  tableName: Schema.Types.String
+  tableName: Schema.Types.String,
 });
 
 const MongoTournamentTables = mongoose.model<MongoTournamentTables>('TournamentTables', TournamentTablesSchema);
 
 function mongoDocumentToDomain(mongoDocument: MongoTournamentTables[]): TournamentTable[] {
-  return mongoDocument.map(document => new TournamentTable({
-    tournamentId: document.tournamentId,
-    tableNumber: TableNumber.from(document.tableNumber),
-    tableName: document.tableName
-  }));
+  return mongoDocument.map(
+    (document) =>
+      new TournamentTable({
+        tournamentId: document.tournamentId,
+        tableNumber: TableNumber.from(document.tableNumber),
+        tableName: document.tableName,
+      }),
+  );
 }
