d53d585b01c652f59af3a5db958600f2097f9c7e
PiotrWR <pwrynio@gmail.com>
2021-03-20 21:59:02 +0100

    Add test description

--- backend/test/modules/tournament-tree/core/domain/TournamentTree.spec.ts
+++ backend/test/modules/tournament-tree/core/domain/TournamentTree.spec.ts
@@ -5,124 +5,126 @@ import { FightingTeamsGroup } from '../../../../../src/modules/tournament-tree/c
 import { FightingTeamsGroupId } from '../../../../../src/modules/tournament-tree/core/domain/FightingTeamsGroupId';
 
 describe('TournamentTree', () => {
-  const testTournamentId = NumberIdGeneratorStub(1000, 'testTournamentId');
+  describe('Single Tournament Tree', () => {
+    const testTournamentId = NumberIdGeneratorStub(1000, 'testTournamentId');
 
-  it('CreateTournamentTree | Create correct 4 teams tree', async () => {
-    //Given
-    const teamEntityIdGen = NumberIdGeneratorStub(1000, 'team');
-    const matchEntityIdGen = NumberIdGeneratorStub(1000, 'match');
-    const tournamentTeamsList = generateTournamentTeamsList(teamEntityIdGen, 4);
+    it('CreateTournamentTree | Create correct 4 teams tree', async () => {
+      //Given
+      const teamEntityIdGen = NumberIdGeneratorStub(1000, 'team');
+      const matchEntityIdGen = NumberIdGeneratorStub(1000, 'match');
+      const tournamentTeamsList = generateTournamentTeamsList(teamEntityIdGen, 4);
 
-    // //When
-    const tournamentTree: TournamentTree = TournamentTree.createSingleTournamentTree({
-      tournamentId: testTournamentId.generate(),
-      tournamentTeams: tournamentTeamsList,
-      entityIdGenerator: matchEntityIdGen,
-    });
-    const expectedWinnerTree: FightingTeamsGroup[] = [
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_3'),
-        firstTeam: tournamentTeamsList[0],
-        secondTeam: tournamentTeamsList[3],
-        fightingTeamsGroupLevel: 0,
-        nextMatchId: FightingTeamsGroupId.from('match_1'),
-        matchNumberInSequence: 1,
-      },
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_2'),
-        firstTeam: tournamentTeamsList[2],
-        secondTeam: tournamentTeamsList[1],
-        fightingTeamsGroupLevel: 0,
-        nextMatchId: FightingTeamsGroupId.from('match_1'),
-        matchNumberInSequence: 2,
-      },
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_1'),
-        firstTeam: undefined,
-        secondTeam: undefined,
-        fightingTeamsGroupLevel: 1,
-        nextMatchId: undefined,
-        matchNumberInSequence: 3,
-      },
-    ].map((item) => FightingTeamsGroup.fromObj(item));
+      // //When
+      const tournamentTree: TournamentTree = TournamentTree.createSingleTournamentTree({
+        tournamentId: testTournamentId.generate(),
+        tournamentTeams: tournamentTeamsList,
+        entityIdGenerator: matchEntityIdGen,
+      });
+      const expectedWinnerTree: FightingTeamsGroup[] = [
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_3'),
+          firstTeam: tournamentTeamsList[0],
+          secondTeam: tournamentTeamsList[3],
+          fightingTeamsGroupLevel: 0,
+          nextMatchId: FightingTeamsGroupId.from('match_1'),
+          matchNumberInSequence: 1,
+        },
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_2'),
+          firstTeam: tournamentTeamsList[2],
+          secondTeam: tournamentTeamsList[1],
+          fightingTeamsGroupLevel: 0,
+          nextMatchId: FightingTeamsGroupId.from('match_1'),
+          matchNumberInSequence: 2,
+        },
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_1'),
+          firstTeam: undefined,
+          secondTeam: undefined,
+          fightingTeamsGroupLevel: 1,
+          nextMatchId: undefined,
+          matchNumberInSequence: 3,
+        },
+      ].map((item) => FightingTeamsGroup.fromObj(item));
 
-    // Then
-    expect(tournamentTree.getTournamentTreeArray()).toIncludeSameMembers(expectedWinnerTree);
-  });
-
-  it('CreateTournamentTree | Create correct 6 teams tree', async () => {
-    //Given
-    const teamEntityIdGen = NumberIdGeneratorStub(1000, 'team');
-    const matchEntityIdGen = NumberIdGeneratorStub(1000, 'match');
-    const tournamentTeamsList = generateTournamentTeamsList(teamEntityIdGen, 6);
-
-    // //When
-    const tournamentTree: TournamentTree = TournamentTree.createSingleTournamentTree({
-      tournamentId: testTournamentId.generate(),
-      tournamentTeams: tournamentTeamsList,
-      entityIdGenerator: matchEntityIdGen,
+      // Then
+      expect(tournamentTree.getTournamentTreeArray()).toIncludeSameMembers(expectedWinnerTree);
     });
 
-    const expectedWinnerTree: FightingTeamsGroup[] = [
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_7'),
-        firstTeam: tournamentTeamsList[0],
-        secondTeam: undefined,
-        fightingTeamsGroupLevel: 0,
-        nextMatchId: FightingTeamsGroupId.from('match_3'),
-        matchNumberInSequence: 1,
-      },
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_6'),
-        firstTeam: tournamentTeamsList[4],
-        secondTeam: tournamentTeamsList[3],
-        fightingTeamsGroupLevel: 0,
-        nextMatchId: FightingTeamsGroupId.from('match_3'),
-        matchNumberInSequence: 2,
-      },
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_5'),
-        firstTeam: tournamentTeamsList[2],
-        secondTeam: tournamentTeamsList[5],
-        fightingTeamsGroupLevel: 0,
-        nextMatchId: FightingTeamsGroupId.from('match_2'),
-        matchNumberInSequence: 3,
-      },
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_4'),
-        firstTeam: undefined,
-        secondTeam: tournamentTeamsList[1],
-        fightingTeamsGroupLevel: 0,
-        nextMatchId: FightingTeamsGroupId.from('match_2'),
-        matchNumberInSequence: 4,
-      },
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_3'),
-        firstTeam: undefined,
-        secondTeam: undefined,
-        fightingTeamsGroupLevel: 1,
-        nextMatchId: FightingTeamsGroupId.from('match_1'),
-        matchNumberInSequence: 5,
-      },
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_2'),
-        firstTeam: undefined,
-        secondTeam: undefined,
-        fightingTeamsGroupLevel: 1,
-        nextMatchId: FightingTeamsGroupId.from('match_1'),
-        matchNumberInSequence: 6,
-      },
-      {
-        fightingTeamsGroupId: FightingTeamsGroupId.from('match_1'),
-        firstTeam: undefined,
-        secondTeam: undefined,
-        fightingTeamsGroupLevel: 2,
-        nextMatchId: undefined,
-        matchNumberInSequence: 7,
-      },
-    ].map((item) => FightingTeamsGroup.fromObj(item));
+    it('CreateTournamentTree | Create correct 6 teams tree', async () => {
+      //Given
+      const teamEntityIdGen = NumberIdGeneratorStub(1000, 'team');
+      const matchEntityIdGen = NumberIdGeneratorStub(1000, 'match');
+      const tournamentTeamsList = generateTournamentTeamsList(teamEntityIdGen, 6);
 
-    // Then
-    expect(tournamentTree.getTournamentTreeArray()).toIncludeSameMembers(expectedWinnerTree);
+      // //When
+      const tournamentTree: TournamentTree = TournamentTree.createSingleTournamentTree({
+        tournamentId: testTournamentId.generate(),
+        tournamentTeams: tournamentTeamsList,
+        entityIdGenerator: matchEntityIdGen,
+      });
+
+      const expectedWinnerTree: FightingTeamsGroup[] = [
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_7'),
+          firstTeam: tournamentTeamsList[0],
+          secondTeam: undefined,
+          fightingTeamsGroupLevel: 0,
+          nextMatchId: FightingTeamsGroupId.from('match_3'),
+          matchNumberInSequence: 1,
+        },
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_6'),
+          firstTeam: tournamentTeamsList[4],
+          secondTeam: tournamentTeamsList[3],
+          fightingTeamsGroupLevel: 0,
+          nextMatchId: FightingTeamsGroupId.from('match_3'),
+          matchNumberInSequence: 2,
+        },
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_5'),
+          firstTeam: tournamentTeamsList[2],
+          secondTeam: tournamentTeamsList[5],
+          fightingTeamsGroupLevel: 0,
+          nextMatchId: FightingTeamsGroupId.from('match_2'),
+          matchNumberInSequence: 3,
+        },
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_4'),
+          firstTeam: undefined,
+          secondTeam: tournamentTeamsList[1],
+          fightingTeamsGroupLevel: 0,
+          nextMatchId: FightingTeamsGroupId.from('match_2'),
+          matchNumberInSequence: 4,
+        },
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_3'),
+          firstTeam: undefined,
+          secondTeam: undefined,
+          fightingTeamsGroupLevel: 1,
+          nextMatchId: FightingTeamsGroupId.from('match_1'),
+          matchNumberInSequence: 5,
+        },
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_2'),
+          firstTeam: undefined,
+          secondTeam: undefined,
+          fightingTeamsGroupLevel: 1,
+          nextMatchId: FightingTeamsGroupId.from('match_1'),
+          matchNumberInSequence: 6,
+        },
+        {
+          fightingTeamsGroupId: FightingTeamsGroupId.from('match_1'),
+          firstTeam: undefined,
+          secondTeam: undefined,
+          fightingTeamsGroupLevel: 2,
+          nextMatchId: undefined,
+          matchNumberInSequence: 7,
+        },
+      ].map((item) => FightingTeamsGroup.fromObj(item));
+
+      // Then
+      expect(tournamentTree.getTournamentTreeArray()).toIncludeSameMembers(expectedWinnerTree);
+    });
   });
 });
