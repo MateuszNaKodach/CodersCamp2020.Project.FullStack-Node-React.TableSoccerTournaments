8f2f413e88158e3d797a9c1e38f586a9a999db88
PiotrWR <pwrynio@gmail.com>
2021-03-22 23:35:32 +0100

    Add index.css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FooTouer from './components/FooTouer/FooTouer';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
