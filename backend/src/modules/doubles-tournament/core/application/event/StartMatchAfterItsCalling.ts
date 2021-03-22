f2cb89e9fd6e06a96c115b789a8a93d7b9313031
Paweł <p.szambelan1@gmail.com>
2021-03-22 22:32:19 +0100

    #90 calling match event handler (#91)

---
+++ backend/src/modules/doubles-tournament/core/application/event/StartMatchAfterItsCalling.ts
@@ -0,0 +1,20 @@
+import { EventHandler } from '../../../../../shared/core/application/event/EventHandler';
+import { MatchWasCalled } from '../../domain/event/MatchWasCalled';
+import { CommandPublisher } from '../../../../../shared/core/application/command/CommandBus';
+import { StartMatch } from '../../../../match-module/core/application/command/StartMatch';
+import { MatchId } from '../../domain/MatchId';
+
+export class StartMatchAfterItsCalling implements EventHandler<MatchWasCalled> {
+  constructor(private readonly commandPublisher: CommandPublisher) {}
+
+  async handle(event: MatchWasCalled): Promise<void> {
+    const matchId = MatchId.from(event.tournamentId, event.calledMatch.matchNumber);
+    await this.commandPublisher.execute(
+      new StartMatch({
+        matchId: matchId.raw,
+        firstMatchSideId: event.calledMatch.team1Id,
+        secondMatchSideId: event.calledMatch.team2Id,
+      }),
+    );
+  }
+}
