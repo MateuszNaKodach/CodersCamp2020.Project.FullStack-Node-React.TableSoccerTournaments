e0bbab221bd07961517df35f21a0946869d6757b
PiotrWR <pwrynio@gmail.com>
2021-03-22 23:56:22 +0100

    Add button files

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,13 +1,12 @@
-import { Button as MaterialUiButton} from '@material-ui/core';
+import {Button as MaterialUiButton} from '@material-ui/core';
 import './Button.css';
 
-function Button() {
+function Button( props:{ name: string }) {
     return (
         <>
-            <MaterialUiButton color="primary" variant="contained">Hello World</MaterialUiButton>
+            <MaterialUiButton color="primary" variant="contained">{props.name}</MaterialUiButton>
         </>
     );
 }
 
 export default Button;
- 
--- frontend/src/components/FooTouer/FooTouer.tsx
+++ frontend/src/components/FooTouer/FooTouer.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './FooTouer.css';
+import Button from "../Button/Button";
 
 function FooTouer() {
-  return (
-    <div className="FooTouer">
-      HelloWorld
-    </div>
-  );
+    return (
+        <>
+            <div className="FooTouer">
+                HelloWorld
+            </div>
+            <Button name={"Hello button"}/>
+        </>
+    );
 }
 
 export default FooTouer;
