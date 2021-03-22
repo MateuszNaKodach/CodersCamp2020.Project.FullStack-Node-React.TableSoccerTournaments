884c54b696a1fdc170e3243ac28375f54747e38c
PiotrWR <pwrynio@gmail.com>
2021-03-22 23:41:30 +0100

    Add button files

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import { Button as MaterialUiButton} from '@material-ui/core';
+import './Button.css';
+
+function Button() {
+    return (
+        <>
+            <MaterialUiButton color="primary" variant="contained">Hello World</MaterialUiButton>
+        </>
+    );
+}
+
+export default Button;
+ 
