14efd969d387cec984af9d3790f162225ffe8428
PiotrWR <pwrynio@gmail.com>
2021-03-23 00:00:17 +0100

    Add onClick to Btn

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import {Button as MaterialUiButton} from '@material-ui/core';
 import './Button.css';
 
-function Button( props:{ name: string }) {
+function Button( props:{ name: string, onClick:()=>{} }) {
     return (
         <>
             <MaterialUiButton color="primary" variant="contained">{props.name}</MaterialUiButton>
