827f8cc2f682016bb9e11ccc478e6d721462fac3
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-03-24 18:19:39 +0100

    Add types for jest-dom expect

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,7 @@ import TourDeFoos from "./components/FooTouer/TourDeFoos";
 //Disable when you want to interact with real REST API server
 if (process.env.REACT_APP_MOCK_WITH_MSW === "true") {
   const { worker } = require("./mocks/msw/browser");
-  worker.start().then(() => console.log("MSW worked started!"));
+  worker.start().then(() => console.log("MSW worker started!"));
 }
 
 ReactDOM.render(
