d3bc867461092ba013246558d973dcf622d8cb70
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-03-24 15:08:24 +0100

    CI env variable on frontend test workflow

--- frontend/src/components/FooTouer/TourDeFoos.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import TourDeFoos from "./TourDeFoos";
-import "@testing-library/jest-dom";
-
-test("renders example text", () => {
-  render(<TourDeFoos />);
-  const linkElement = screen.getByText(/HelloWorld/i);
-  expect(linkElement).toBeInTheDocument();
-});
