9e065de31ff92b24d20d8f98ccf3ce2c7429835a
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-03-24 13:19:36 +0100

    Frontend | Disable MSW

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import TourDeFoos from "./components/FooTouer/TourDeFoos";
 
+//FIXME: Na razie nie działa
 //Disable when you want to interact with real server
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start().then(() => console.log("MSW worked started!"));
-}
+//if (process.env.NODE_ENV === "development") {
+//  const { worker } = require("./mocks/browser");
+//  worker.start().then(() => console.log("MSW worked started!"));
+//}
 
 ReactDOM.render(
   <React.StrictMode>
