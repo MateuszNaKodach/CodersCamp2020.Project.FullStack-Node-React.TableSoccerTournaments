48c0012f7f2b0cab014043d31d63c32e183a4aa9
Tomek <tomasz.dworniczak@hotmail.com>
2021-03-24 12:20:58 +0100

    #71 Change implementation

--- backend/src/modules/doubles-tournament/core/application/event/EnqueueMatchWhenTournamentWasStarted.ts
+++ backend/src/modules/doubles-tournament/core/application/event/EnqueueMatchWhenTournamentWasStarted.ts
@@ -6,27 +6,17 @@ import {TournamentTreeRepository} from "../../../../tournament-tree/core/applica
 export class EnqueueMatchWhenTournamentWasStarted implements EventHandler<TournamentWasStarted> {
     constructor(private readonly commandPublisher: CommandPublisher, private readonly repository: TournamentTreeRepository) {}
 
-    /** Nie wiem, które rozwiązanie lepsze */
-
     async handle(event: TournamentWasStarted): Promise<void> {
-        const tournamentTree = await this.repository
-            .findByTournamentTreeId(event.tournamentId)
-            // .then((tournamentTree) => tournamentTree?.getMatchesQueueReadyToBegin().forEach((matchToEnqueue) => {
-            //     this.commandPublisher.execute(new EnqueueMatch({
-            //         tournamentId: event.tournamentId,
-            //         matchNumber: matchToEnqueue.matchNumberInSequence,
-            //         team1Id: matchToEnqueue.firstTeam?.teamId.raw,
-            //         team2Id: matchToEnqueue.secondTeam?.teamId.raw
-            //     }));
-            // }));
+        const tournamentTree = await this.repository.findByTournamentTreeId(event.tournamentId);
 
         const matchesToEnqueue = tournamentTree?.getMatchesQueueReadyToBegin()
-            .forEach((matchToEnqueue) => {
+
+        matchesToEnqueue?.forEach((matchToEnqueue) => {
                 this.commandPublisher.execute(new EnqueueMatch({
                     tournamentId: event.tournamentId,
-                    matchNumber: matchToEnqueue.matchNumberInSequence as number,
-                    team1Id: matchToEnqueue.firstTeam?.teamId.raw as string,
-                    team2Id: matchToEnqueue.secondTeam?.teamId.raw as string
+                    matchNumber: matchToEnqueue.matchNumberInSequence,
+                    team1Id: matchToEnqueue.firstTeam?.teamId.raw,
+                    team2Id: matchToEnqueue.secondTeam?.teamId.raw
                 }));
             });
     }
