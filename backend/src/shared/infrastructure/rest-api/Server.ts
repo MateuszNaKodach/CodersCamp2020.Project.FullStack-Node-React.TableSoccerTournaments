743b0e0d1bb527af2d89414e34204b8d77c5a645
Mateusz Nowak <kontakt.mateusznowak@gmail.com>
2021-03-24 19:35:36 +0100

    #101 TournamentRegistrations | Searching for players to register
    - Configure MSW - Mock Service Worker
    - Create sample component for registering players on Tournament

--- backend/src/shared/infrastructure/rest-api/Server.ts
+++ backend/src/shared/infrastructure/rest-api/Server.ts
@@ -3,11 +3,13 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import { ModuleRestApi } from '../../presentation/rest-api/ModuleRestApi';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 export function restApiExpressServer(modules: ModuleRestApi[] = []): Express {
   const swaggerDocument = YAML.load('./src/rest-api-docs.yaml');
   const server = express();
   server.use(bodyParser.json());
+  server.use(cors());
 
   modules.forEach((restApiModule) => {
     server.use('/rest-api' + restApiModule.path, restApiModule.router);
--- frontend/src/components/FooTouer/FooTouer.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import FooTouer from './FooTouer';
-import '@testing-library/jest-dom';
-
-test('renders example text', () => {
-  render(<FooTouer />);
-  const linkElement = screen.getByText(/HelloWorld/i);
-  expect(linkElement).toBeInTheDocument();
-});
