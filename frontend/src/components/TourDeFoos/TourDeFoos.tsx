30c9f74307207625048342492a3f286b24b4a6e2
PiotrWR <pwrynio@gmail.com>
2021-03-27 20:46:55 +0100

    Add first routers - first version

--- frontend/src/components/TourDeFoos/TourDeFoos.tsx
+++ frontend/src/components/TourDeFoos/TourDeFoos.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
 import {Menu} from '../Menu';
 import {grey} from "@material-ui/core/colors";
 import {Centered} from "../Shared/Centered";
 import {VerticalSpace} from "../Shared/VerticalSpace";
-import {APP_NAME} from "../../constants/constants";
+import {APP_NAME} from "../../constants/names";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {
+    PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW,
+    PATH_FOR_LOGIN_VIEW, PATH_FOR_PLAYER_PROFILE_CREATION_VIEW, PATH_FOR_PLAYERS_PROFILES_VIEW,
+    PATH_FOR_START_VIEW, PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW,
+    PATH_FOR_TOURNAMENTS_SELECTION_VIEW
+} from "../../constants/paths";
 
 const theme = createMuiTheme({
     palette: {
@@ -32,17 +39,34 @@ function TourDeFoos() {
     ];
 
     return (
-        <MuiThemeProvider theme={theme}>
+        <Router>
+            <MuiThemeProvider theme={theme}>
 
-            <Centered>
-                <VerticalSpace height="1rem"/>
-
-                <Menu title={APP_NAME} menuButtonsList={menuButtonsListProps} />
-                <VerticalSpace height="1rem"/>
-
-
-            </Centered>
-        </MuiThemeProvider>
+                <Switch>
+                    <Centered>
+                        <Route path={PATH_FOR_START_VIEW}>
+                            <VerticalSpace height="1rem"/>
+                            <Menu title={APP_NAME} menuButtonsList={menuButtonsListProps}/>
+                            <VerticalSpace height="1rem"/>
+                        </Route>
+                        <Route path={PATH_FOR_LOGIN_VIEW}>
+                        </Route>
+                        <Route path={PATH_FOR_LOGIN_VIEW}>
+                        </Route>
+                        <Route path={PATH_FOR_TOURNAMENTS_SELECTION_VIEW}>
+                        </Route>
+                        <Route path={PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW}>
+                        </Route>
+                        <Route path={PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW}>
+                        </Route>
+                        <Route path={PATH_FOR_PLAYERS_PROFILES_VIEW}>
+                        </Route>
+                        <Route path={PATH_FOR_PLAYER_PROFILE_CREATION_VIEW}>
+                        </Route>
+                    </Centered>
+                </Switch>
+            </MuiThemeProvider>
+        </Router>
     );
 }
 
