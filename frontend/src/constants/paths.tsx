2c70d117ef8092f2ce1c51054e69fd3f3a269540
PiotrWR <pwrynio@gmail.com>
2021-03-27 20:46:11 +0100

    Add paths

---
+++ frontend/src/constants/paths.tsx
@@ -0,0 +1,7 @@
+export  const PATH_FOR_START_VIEW : string = "/";
+export const PATH_FOR_LOGIN_VIEW : string = "/";
+export  const PATH_FOR_TOURNAMENTS_SELECTION_VIEW : string = "/";
+export  const PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW : string = "/";
+export  const PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW : string = "/";
+export  const PATH_FOR_PLAYERS_PROFILES_VIEW : string = "/";
+export  const PATH_FOR_PLAYER_PROFILE_CREATION_VIEW : string = "/";
