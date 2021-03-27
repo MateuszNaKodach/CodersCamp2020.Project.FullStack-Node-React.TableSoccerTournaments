54dadebc09e350939023bae3005e7d4944815339
PiotrWR <pwrynio@gmail.com>
2021-03-27 20:49:21 +0100

    Update paths

--- frontend/src/constants/paths.tsx
+++ frontend/src/constants/paths.tsx
@@ -1,7 +1,7 @@
 export  const PATH_FOR_START_VIEW : string = "/";
-export const PATH_FOR_LOGIN_VIEW : string = "/";
-export  const PATH_FOR_TOURNAMENTS_SELECTION_VIEW : string = "/";
-export  const PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW : string = "/";
-export  const PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW : string = "/";
-export  const PATH_FOR_PLAYERS_PROFILES_VIEW : string = "/";
-export  const PATH_FOR_PLAYER_PROFILE_CREATION_VIEW : string = "/";
+export const PATH_FOR_LOGIN_VIEW : string = "/login";
+export  const PATH_FOR_TOURNAMENTS_SELECTION_VIEW : string = "/tournaments-selection";
+export  const PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW : string = "/new-tournament";
+export  const PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW : string = "/tournament-registration";
+export  const PATH_FOR_PLAYERS_PROFILES_VIEW : string = "/players-profiles";
+export  const PATH_FOR_PLAYER_PROFILE_CREATION_VIEW : string = "/new-player-profiles";
