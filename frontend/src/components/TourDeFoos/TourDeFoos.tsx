bd50d3f1db25aadfbe9ec9198eff067d640c1f76
PiotrWR <pwrynio@gmail.com>
2021-03-28 03:58:04 +0200

    Add router

--- frontend/src/components/TourDeFoos/TourDeFoos.tsx
+++ frontend/src/components/TourDeFoos/TourDeFoos.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+import {Button, createMuiTheme, MuiThemeProvider} from "@material-ui/core";
 import {Menu} from '../Menu';
 import {grey} from "@material-ui/core/colors";
 import {Centered} from "../Shared/Centered";
 import {VerticalSpace} from "../Shared/VerticalSpace";
 import {APP_NAME} from "../../constants/names";
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import {
     PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW,
     PATH_FOR_LOGIN_VIEW, PATH_FOR_PLAYER_PROFILE_CREATION_VIEW, PATH_FOR_PLAYERS_PROFILES_VIEW,
-    PATH_FOR_START_VIEW, PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW,
+    PATH_FOR_HOME_VIEW, PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW,
     PATH_FOR_TOURNAMENTS_SELECTION_VIEW
 } from "../../constants/paths";
 
@@ -29,42 +29,46 @@ function TourDeFoos() {
             onClick: () => {
                 console.log("Menu button was clicked!")
             },
+            onLink: PATH_FOR_TOURNAMENTS_SELECTION_VIEW
         },
         {
             textName: "Zawodnicy",
             onClick: () => {
                 console.log("Menu button was clicked!")
             },
+            onLink: PATH_FOR_PLAYERS_PROFILES_VIEW
         }
     ];
 
     return (
         <Router>
             <MuiThemeProvider theme={theme}>
+                <Centered>
+                    <Switch>
+                        <Route path={PATH_FOR_LOGIN_VIEW} exact>
+                            <Button>PATH_FOR_LOGIN_VIEW</Button>
+                        </Route>
+                        <Route path={PATH_FOR_TOURNAMENTS_SELECTION_VIEW} exact>
+                            <Button>PATH_FOR_TOURNAMENTS_SELECTION_VIEW</Button>
+                        </Route>
 
-                <Switch>
-                    <Centered>
-                        <Route path={PATH_FOR_START_VIEW}>
+                        <Route path={PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW} exact>
+                        </Route>
+                        <Route path={PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW} exact>
+                        </Route>
+                        <Route path={PATH_FOR_PLAYERS_PROFILES_VIEW} exact>
+                        </Route>
+                        <Route path={PATH_FOR_PLAYER_PROFILE_CREATION_VIEW} exact>
+                        </Route>
+
+                        <Route path={PATH_FOR_HOME_VIEW} exact>
                             <VerticalSpace height="1rem"/>
                             <Menu title={APP_NAME} menuButtonsList={menuButtonsListProps}/>
                             <VerticalSpace height="1rem"/>
                         </Route>
-                        <Route path={PATH_FOR_LOGIN_VIEW}>
-                        </Route>
-                        <Route path={PATH_FOR_LOGIN_VIEW}>
-                        </Route>
-                        <Route path={PATH_FOR_TOURNAMENTS_SELECTION_VIEW}>
-                        </Route>
-                        <Route path={PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW}>
-                        </Route>
-                        <Route path={PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW}>
-                        </Route>
-                        <Route path={PATH_FOR_PLAYERS_PROFILES_VIEW}>
-                        </Route>
-                        <Route path={PATH_FOR_PLAYER_PROFILE_CREATION_VIEW}>
-                        </Route>
-                    </Centered>
-                </Switch>
+
+                    </Switch>
+                </Centered>
             </MuiThemeProvider>
         </Router>
     );
--- frontend/src/constants/paths.tsx
+++ frontend/src/constants/paths.tsx
@@ -1,4 +1,4 @@
-export  const PATH_FOR_START_VIEW : string = "/";
+export  const PATH_FOR_HOME_VIEW : string = "/";
 export const PATH_FOR_LOGIN_VIEW : string = "/login";
 export  const PATH_FOR_TOURNAMENTS_SELECTION_VIEW : string = "/tournaments-selection";
 export  const PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW : string = "/new-tournament";
