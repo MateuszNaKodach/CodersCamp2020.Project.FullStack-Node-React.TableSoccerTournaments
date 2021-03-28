846dc878f36474b748521c0935a0960ad7f292bc
Piotr Witold Rynio <41823689+PiotrWR@users.noreply.github.com>
2021-03-28 17:09:23 +0200

    #106 React router (#106)
    * Add paths
    
    * Add first routers - first version
    
    * Update paths
    
    * Add router
    
    * Fix tests
    
    * Add names of view

--- frontend/src/components/TourDeFoos/TourDeFoos.tsx
+++ frontend/src/components/TourDeFoos/TourDeFoos.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import { createMuiTheme, MuiThemeProvider} from "@material-ui/core";
-import {Menu} from '../Menu';
+import {Button, createMuiTheme, MuiThemeProvider} from "@material-ui/core";
 import {grey} from "@material-ui/core/colors";
 import {Centered} from "../Shared/Centered";
-import {VerticalSpace} from "../Shared/VerticalSpace";
-import {APP_NAME} from "../../constants/constants";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {
+    PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW,
+    PATH_FOR_LOGIN_VIEW, PATH_FOR_PLAYER_PROFILE_CREATION_VIEW, PATH_FOR_PLAYERS_PROFILES_VIEW,
+    PATH_FOR_HOME_VIEW, PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW,
+    PATH_FOR_TOURNAMENTS_SELECTION_VIEW
+} from "../../constants/paths";
+import {HomeMenu} from "../HomeMenu/HomeMenu";
 
 const theme = createMuiTheme({
     palette: {
@@ -16,33 +21,37 @@ const theme = createMuiTheme({
 });
 
 function TourDeFoos() {
-    const menuButtonsListProps = [
-        {
-            textName: "Turniej",
-            onClick: () => {
-                console.log("Menu button was clicked!")
-            },
-        },
-        {
-            textName: "Zawodnicy",
-            onClick: () => {
-                console.log("Menu button was clicked!")
-            },
-        }
-    ];
-
     return (
-        <MuiThemeProvider theme={theme}>
+        <Router>
+            <MuiThemeProvider theme={theme}>
+                <Centered>
+                    <Switch>
+                        <Route path={PATH_FOR_LOGIN_VIEW} exact>
+                            <Button>PATH_FOR_LOGIN_VIEW</Button>
+                        </Route>
+                        <Route path={PATH_FOR_TOURNAMENTS_SELECTION_VIEW} exact>
+                            PATH_FOR_TOURNAMENTS_SELECTION_VIEW
+                        </Route>
+                        <Route path={PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW} exact>
+                            PATH_FOR_CREATING_NEW_TOURNAMENT_VIEW
+                        </Route>
+                        <Route path={PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW} exact>
+                            PATH_FOR_TOURNAMENT_REGISTRATIONS_VIEW
+                        </Route>
+                        <Route path={PATH_FOR_PLAYERS_PROFILES_VIEW} exact>
+                            PATH_FOR_PLAYERS_PROFILES_VIEW
+                        </Route>
+                        <Route path={PATH_FOR_PLAYER_PROFILE_CREATION_VIEW} exact>
+                            PATH_FOR_PLAYER_PROFILE_CREATION_VIEW
+                        </Route>
 
-            <Centered>
-                <VerticalSpace height="1rem"/>
-
-                <Menu title={APP_NAME} menuButtonsList={menuButtonsListProps} />
-                <VerticalSpace height="1rem"/>
-
-
-            </Centered>
-        </MuiThemeProvider>
+                        <Route path={PATH_FOR_HOME_VIEW} exact>
+                            <HomeMenu/>
+                        </Route>
+                    </Switch>
+                </Centered>
+            </MuiThemeProvider>
+        </Router>
     );
 }
 
