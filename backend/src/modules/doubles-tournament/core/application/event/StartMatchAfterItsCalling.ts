72d9f8544b5c54b0672bfd06021ac6f55cf3a284
lamparina <anna.lamperska@gmail.com>
2021-03-29 20:22:24 +0200

    Remove test for not existing method

--- backend/src/modules/doubles-tournament/core/application/event/StartMatchAfterItsCalling.ts
+++ backend/src/modules/doubles-tournament/core/application/event/StartMatchAfterItsCalling.ts
@@ -20,13 +20,13 @@ export class StartMatchAfterItsCalling implements EventHandler<MatchWasCalled> {
 
   async handle(event: MatchWasCalled): Promise<void> {
     const tournamentId = TournamentId.from(event.tournamentId);
-    const startedMatch: QueuedMatch = {
+    const startedMatch = new QueuedMatch ({
       matchNumber: MatchNumber.from(event.calledMatch.matchNumber),
       team1Id: TeamId.from(event.calledMatch.team1Id),
       team2Id: TeamId.from(event.calledMatch.team2Id),
       tableNumber: event.tableNumber,
       started: true,
-    };
+    });
     const matchesQueue = await this.matchesQueueRepository.findByTournamentId(tournamentId.raw);
     const queue = startMatchInMatchesQueue(tournamentId, startedMatch, matchesQueue);
     await this.matchesQueueRepository.save(queue);
