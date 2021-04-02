45383ca2990c1b04390a40d4fd40b4afafe711ba
DomiZet <57037612+DomiZet@users.noreply.github.com>
2021-04-02 16:30:14 +0200

    Adjust Footer's size (#125)

--- frontend/src/components/molecules/Footer/styles.js
+++ frontend/src/components/molecules/Footer/styles.js
@@ -7,7 +7,7 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: theme.palette.primary.main,
         color: theme.palette.primary.contrastText,
         padding: '10px 0',
-        width: '20%'
+        width: '100%'
     },
 }));
 
