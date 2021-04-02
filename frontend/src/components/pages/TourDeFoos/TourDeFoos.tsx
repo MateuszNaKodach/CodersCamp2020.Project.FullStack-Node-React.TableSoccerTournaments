d5b0beb59faabd77c900177c9fd93574b0b86b06
Piotr Witold Rynio <41823689+PiotrWR@users.noreply.github.com>
2021-04-03 00:48:47 +0200

    #133 Fix footer css and give to correct project catalog
    Fix footer css and give to correct project catalog

--- frontend/src/components/pages/TourDeFoos/TourDeFoos.tsx
+++ frontend/src/components/pages/TourDeFoos/TourDeFoos.tsx
@@ -10,7 +10,9 @@ import {
 } from "../../atoms/constants/paths";
 import {HomeMenu} from "../HomeMenu/HomeMenu";
 import {TournamentRegistrations} from "../TournamentRegistrations";
-import Footer from "../../molecules/Footer/Footer";
+import Footer from "../../organisms/Footer/Footer";
+import {VerticalSpace} from "../../atoms/Shared/VerticalSpace";
+import React from "react";
 
 const theme = createMuiTheme({
     palette: {
@@ -52,6 +54,7 @@ function TourDeFoos() {
                             <HomeMenu/>
                         </Route>
                     </Switch>
+                    <VerticalSpace height="35px"/>
                     <Footer />
                 </Centered>
             </MuiThemeProvider>
