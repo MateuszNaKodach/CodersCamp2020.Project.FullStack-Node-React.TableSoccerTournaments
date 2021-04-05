37e8f0c406860911a3993d63e8225c1b65074013
Paweł <p.szambelan1@gmail.com>
2021-04-05 11:49:53 +0200

    #104 player profile creation form bottomsheet (#107)

---
+++ frontend/src/components/organisms/AddingPlayerForm/AddingPlayerForm.test.tsx
@@ -0,0 +1,69 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { TournamentRegistrations } from "../../pages/TournamentRegistrations";
+import { PlayerProfileDto } from "../../../restapi/players-profiles";
+import { server } from "../../../mocks/msw/server";
+import { rest } from "msw";
+import userEvent from "@testing-library/user-event";
+
+describe("Adding player form", () => {
+  it("after clicking button form should be shown and display title, inputs and button", async () => {
+    //Given
+    const playersProfiles: PlayerProfileDto[] = [];
+    getPlayersProfilesWillReturn(playersProfiles);
+
+    render(
+      <Router>
+        <TournamentRegistrations />
+      </Router>
+    );
+    await waitForElementToBeRemoved(() =>
+      screen.getByTestId("TournamentRegistrationsLoadingIndicator")
+    );
+    const searchPlayerInput = await screen.findByLabelText("Zawodnik");
+    userEvent.clear(searchPlayerInput);
+    userEvent.type(searchPlayerInput, "Mufasa");
+
+    //When
+    const registerNewPlayerForTournamentButton = await screen.findByText(
+      "Dodaj i zapisz"
+    );
+    userEvent.click(registerNewPlayerForTournamentButton);
+
+    //Then
+    const newPlayerText = await screen.findByText("Nowy zawodnik");
+    expect(newPlayerText).toBeInTheDocument();
+
+    const savePlayerButton = await screen.findByText("Zapisz zawodnika");
+    expect(savePlayerButton).toBeInTheDocument();
+
+    const nameInput = await screen.findByLabelText("Imię");
+    expect(nameInput).toBeInTheDocument();
+
+    const surnameInput = await screen.findByLabelText("Nazwisko");
+    expect(surnameInput).toBeInTheDocument();
+
+    const email = await screen.findByLabelText("Adres e-mail");
+    expect(email).toBeInTheDocument();
+
+    const phone = await screen.findByLabelText("Numer telefonu");
+    expect(phone).toBeInTheDocument();
+  });
+});
+
+function getPlayersProfilesWillReturn(playersProfiles: PlayerProfileDto[]) {
+  server.use(
+    rest.get("*/rest-api/players-profiles", (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          items: playersProfiles,
+        })
+      );
+    })
+  );
+}
