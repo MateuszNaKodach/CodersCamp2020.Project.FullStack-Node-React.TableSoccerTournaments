b7a4ecd4e4ddf5dff3454edcc2f25062e519ac61
PiotrWR <pwrynio@gmail.com>
2021-04-08 23:51:57 +0200

    Initial commit and format all files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,13 +5,13 @@ import './index.css';
 
 //Disable when you want to interact with real REST API server
 if (process.env.REACT_APP_MOCK_WITH_MSW === "true") {
-  const { worker } = require("./mocks/msw/browser");
-  worker.start().then(() => console.log("MSW worker started!"));
+    const {worker} = require("./mocks/msw/browser");
+    worker.start().then(() => console.log("MSW worker started!"));
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <TourDeFoos />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <TourDeFoos/>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
