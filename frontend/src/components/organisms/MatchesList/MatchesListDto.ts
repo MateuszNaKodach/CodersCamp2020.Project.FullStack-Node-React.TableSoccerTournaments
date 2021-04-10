4dc8b58bdcb4bbe702c84f744fc730e560a344cc
PiotrWR <pwrynio@gmail.com>
2021-04-10 22:38:56 +0200

    Add MatchesListRestApi And MatchesListDto

---
+++ frontend/src/components/organisms/MatchesList/MatchesListDto.ts
@@ -0,0 +1,12 @@
+export type MatchesListDto = {
+  readonly tournamentId: "094469cf-6fcc-4947-a5c0-be1e94f54855";
+  readonly queue: [
+    {
+      readonly matchNumber: 1;
+      readonly team1Id: "b8c4c924-a9a5-4296-a319-ebdfbadec6ba";
+      readonly team12d: "b8c4c924-a9a5-4296-a319-ebdfbadec6ba";
+      readonly tableNumber: 1;
+      readonly started: true;
+    }
+  ];
+};
