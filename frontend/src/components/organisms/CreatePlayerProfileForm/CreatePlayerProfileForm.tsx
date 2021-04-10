8be82bec7e8ca6477a1333278a5f518f1592275e
Paweł <p.szambelan1@gmail.com>
2021-04-10 23:05:18 +0200

    Rename form file and its functions (#150)

--- frontend/src/components/organisms/CreatePlayerProfileForm/CreatePlayerProfileForm.tsx
+++ frontend/src/components/organisms/CreatePlayerProfileForm/CreatePlayerProfileForm.tsx
@@ -20,8 +20,8 @@ const validationSchema = yup.object({
     .required("To pole jest wymagane."),
 });
 
-function AddingPlayerForm(props: {
-  onPlayerAdded: (name: string, surname: string) => void;
+function CreatePlayerProfileForm(props: {
+  onPlayerProfileCreated: (name: string, surname: string) => void;
   tournamentId: string;
 }) {
   const formik = useFormik({
@@ -46,7 +46,7 @@ function AddingPlayerForm(props: {
           tournamentId: props.tournamentId,
           playerId: playerId,
         });
-        props.onPlayerAdded(values.name, values.surname);
+        props.onPlayerProfileCreated(values.name, values.surname);
       } catch (error) {
         alert(error.response.data.message);
       }
@@ -126,4 +126,4 @@ function AddingPlayerForm(props: {
   );
 }
 
-export default AddingPlayerForm;
+export default CreatePlayerProfileForm;
