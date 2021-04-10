4aafb501e52ca8630aceed7cfdf524bea5590ef7
Tomek <tomasz.dworniczak@hotmail.com>
2021-04-10 12:32:35 +0200

    #128 Create tournament card

---
+++ frontend/src/components/molecules/TournamentCard/TournamentCard.tsx
@@ -0,0 +1,48 @@
+import {Card, CardActions, CardContent, CardMedia, Typography} from "@material-ui/core";
+import TextButton from "../../atoms/Shared/TextButton/TextButton";
+import {makeStyles} from "@material-ui/core/styles";
+import {VerticalSpace} from "../../atoms/Shared/VerticalSpace";
+import PlusButton from "../../atoms/Shared/PlusButton/PlusButton";
+
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 300,
+    },
+    media: {
+        height: 150,
+    },
+    cardActions: {
+        justifyContent: "flex-end",
+    }
+});
+
+const TournamentCard = () => {
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media}
+                       image="https://images.unsplash.com/photo-1593958200942-2c05f0a4e72e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+                       title="Tournament Image"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {/*{tournamentName}*/}
+                    Major Wrocław
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    ZAPISY: Zakończone {/*{registrationsStatus}*/}
+                </Typography>
+            </CardContent>
+            <VerticalSpace height={"20px"}/>
+            <CardActions disableSpacing={true} className={classes.cardActions}>
+                <TextButton text={"Zapisy"} onLink={''}/>
+                <TextButton text={"Mecze i wyniki"} onLink={''}/>
+                <PlusButton onLink={''}/>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default TournamentCard;
