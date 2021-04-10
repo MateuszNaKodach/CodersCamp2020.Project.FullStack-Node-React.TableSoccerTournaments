11c01830b8c2b2551d7fd77021de93703aa02ecf
Anna <59768305+lamparina@users.noreply.github.com>
2021-04-10 14:18:06 +0200

    #148 Double Tournament Module | Tournament match ended (#152)
    * Add tests
    
    * Remove unnecessary conditions and TCs
    
    * Publish event TournamentMatchWasEnded when MatchHasEnded
    
    * Reformat code
    
    * Get tournamentId and matchNumber from raw MatchId

---
+++ backend/test/modules/doubles-tournament/core/application/EndTournamentMatch.spec.ts
@@ -0,0 +1,45 @@
+import { CommandBus } from '../../../../../src/shared/core/application/command/CommandBus';
+import { testDoublesTournamentsModule } from './TestDoublesTournamentsModule';
+import { FromListIdGeneratorStub } from '../../../../test-support/shared/core/FromListIdGeneratorStub';
+import { InMemoryCommandBus } from '../../../../../src/shared/infrastructure/core/application/command/InMemoryCommandBus';
+import { MatchHasEnded } from '../../../../../src/modules/match-module/core/domain/event/MatchHasEnded';
+import { StoreAndForwardDomainEventBus } from '../../../../../src/shared/infrastructure/core/application/event/StoreAndForwardDomainEventBus';
+import { InMemoryDomainEventBus } from '../../../../../src/shared/infrastructure/core/application/event/InMemoryDomainEventBus';
+import { testMatchModule } from '../../../match-module/core/application/TestMatchModule';
+import { TestModuleCore } from '../../../../test-support/shared/core/TestModuleCore';
+import { TournamentMatchWasEnded } from '../../../../../src/modules/doubles-tournament/core/domain/event/TournamentMatchWasEnded';
+
+describe('End tournament match', () => {
+  it('When match was ended, then end tournament match', async () => {
+    //Given
+    const team1Id = 'Team1Id';
+    const team2Id = 'Team2Id';
+    const tournamentId = 'TournamentId';
+    const matchNumber = 3;
+    const currentTime = new Date();
+    const commandBus: CommandBus = new InMemoryCommandBus();
+    const eventBus: StoreAndForwardDomainEventBus = new StoreAndForwardDomainEventBus(new InMemoryDomainEventBus());
+    const entityIdGen = FromListIdGeneratorStub([team1Id, team2Id]);
+    const doublesTournament: TestModuleCore = testDoublesTournamentsModule(currentTime, entityIdGen, commandBus, eventBus);
+    const matchModule: TestModuleCore = testMatchModule(currentTime, commandBus, eventBus);
+
+    //When
+    const matchHasEnded = new MatchHasEnded({
+      occurredAt: currentTime,
+      matchId: `${tournamentId}_${matchNumber}`,
+      winnerId: team1Id,
+      looserId: team2Id,
+    });
+    matchModule.publishEvent(matchHasEnded);
+
+    //Then
+    expect(doublesTournament.lastPublishedEvent()).toStrictEqual(
+      new TournamentMatchWasEnded({
+        occurredAt: currentTime,
+        tournamentId: tournamentId,
+        matchNumber: matchNumber,
+        winnerId: team1Id,
+      }),
+    );
+  });
+});
--- backend/test/modules/match-module/core/application/TestMatchModule.ts
+++ backend/test/modules/match-module/core/application/TestMatchModule.ts
@@ -1,8 +1,18 @@
-import { testModuleCore, TestModuleCore } from '../../../../test-support/shared/core/TestModuleCore';
+import { ModuleCoreFactory, testModuleCore, TestModuleCore } from '../../../../test-support/shared/core/TestModuleCore';
 import { MatchModuleCore } from '../../../../../src/modules/match-module/core/MatchModuleCore';
 import { InMemoryMatchRepository } from '../../../../../src/modules/match-module/infrastructure/repository/inmemory/InMemoryMatchRepository';
+import { CommandBus } from '../../../../../src/shared/core/application/command/CommandBus';
+import { InMemoryCommandBus } from '../../../../../src/shared/infrastructure/core/application/command/InMemoryCommandBus';
+import { StoreAndForwardDomainEventBus } from '../../../../../src/shared/infrastructure/core/application/event/StoreAndForwardDomainEventBus';
+import { InMemoryDomainEventBus } from '../../../../../src/shared/infrastructure/core/application/event/InMemoryDomainEventBus';
 
-export function testMatchModule(currentTime: Date): TestModuleCore {
+export function testMatchModule(
+  currentTime: Date,
+  commandBus: CommandBus = new InMemoryCommandBus(),
+  eventBus: StoreAndForwardDomainEventBus = new StoreAndForwardDomainEventBus(new InMemoryDomainEventBus()),
+): TestModuleCore {
   const matchRepository = new InMemoryMatchRepository();
-  return testModuleCore((commandBus, eventBus, queryBus) => MatchModuleCore(eventBus, commandBus, () => currentTime, matchRepository));
+  const moduleCoreFactory: ModuleCoreFactory = (commandBus, eventBus, queryBus) =>
+    MatchModuleCore(eventBus, commandBus, () => currentTime, matchRepository);
+  return testModuleCore(moduleCoreFactory, commandBus, eventBus);
 }
