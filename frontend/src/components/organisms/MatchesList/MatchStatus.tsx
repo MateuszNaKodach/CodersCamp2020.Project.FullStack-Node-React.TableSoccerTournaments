281372e81b6355a43816afb68a4325424008bd7d
PiotrWR <pwrynio@gmail.com>
2021-04-11 03:44:23 +0200

    Update Match Status

--- frontend/src/components/organisms/MatchesList/MatchStatus.tsx
+++ frontend/src/components/organisms/MatchesList/MatchStatus.tsx
@@ -1,6 +1,6 @@
 export enum MatchStatus {
     FINISHED= "Zakończony",
     CALLED= "Wywołany",
-    NOT_TABLE= "Brak stołu",
-    NON_PLAYERS= "Brak graczy",
+    NO_TABLE= "Brak stołu",
+    NO_PLAYERS= "Brak graczy",
 }
