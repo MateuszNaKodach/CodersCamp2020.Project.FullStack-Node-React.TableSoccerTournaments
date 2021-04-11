f2c07005c86292b5e9fda9b74933747f76884a1e
PiotrWR <pwrynio@gmail.com>
2021-04-11 02:07:22 +0200

    Update MatchesListDto.tsx

--- frontend/src/components/organisms/MatchesList/MatchesListDto.tsx
+++ frontend/src/components/organisms/MatchesList/MatchesListDto.tsx
@@ -1,12 +1,12 @@
 export type MatchesListDto = {
-  readonly tournamentId: "094469cf-6fcc-4947-a5c0-be1e94f54855";
+  readonly tournamentId: string;
   readonly queue: [
     {
-      readonly matchNumber: 1;
-      readonly team1Id: "b8c4c924-a9a5-4296-a319-ebdfbadec6ba";
-      readonly team12d: "b8c4c924-a9a5-4296-a319-ebdfbadec6ba";
-      readonly tableNumber: 1;
-      readonly started: true;
+      readonly matchNumber: number;
+      readonly team1Id: string | undefined;
+      readonly team12d: string | undefined;
+      readonly tableNumber: number | undefined;
+      readonly started: boolean;
     }
   ];
 };
