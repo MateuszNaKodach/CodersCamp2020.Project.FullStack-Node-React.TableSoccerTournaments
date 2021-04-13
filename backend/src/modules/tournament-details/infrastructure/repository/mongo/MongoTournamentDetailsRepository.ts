3c1b9123b13e6b1e2e495a1846a111cbf2fd2c35
Tomek <50457289+tomdworniczak@users.noreply.github.com>
2021-04-14 00:40:49 +0200

    Tournament details module (#178)

---
+++ backend/src/modules/tournament-details/infrastructure/repository/mongo/MongoTournamentDetailsRepository.ts
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from 'mongoose';
+import { TournamentDetailsRepository } from '../../../core/application/TournamentDetailsRepository';
+import { TournamentDetails } from '../../../core/domain/TournamentDetails';
+import { TournamentName } from '../../../core/domain/TournamentName';
+
+export class MongoTournamentDetailsRepository implements TournamentDetailsRepository {
+  async findByTournamentId(tournamentId: string): Promise<TournamentDetails | undefined> {
+    const mongoFindResult = await MongoTournamentDetails.findById(tournamentId);
+    return mongoFindResult ? mongoDocumentToDomain(mongoFindResult) : undefined;
+  }
+
+  async save(details: TournamentDetails): Promise<void> {
+    await MongoTournamentDetails.findByIdAndUpdate(
+      { _id: details.tournamentId },
+      {
+        _id: details.tournamentId,
+        name: details.tournamentName.raw,
+      },
+      { upsert: true, useFindAndModify: true },
+    );
+  }
+
+  async findAll(): Promise<TournamentDetails[]> {
+    const mongoFindResult = await MongoTournamentDetails.find();
+    return mongoFindResult.map((mongoDocument) => mongoDocumentToDomain(mongoDocument));
+  }
+}
+
+type MongoTournamentDetails = {
+  readonly _id: string;
+  readonly name: string;
+} & mongoose.Document;
+
+const TournamentDetailsSchema = new mongoose.Schema({
+  _id: Schema.Types.String,
+  name: Schema.Types.String,
+});
+
+const MongoTournamentDetails = mongoose.model<MongoTournamentDetails>('TournamentDetails', TournamentDetailsSchema);
+
+function mongoDocumentToDomain(mongoDocument: MongoTournamentDetails): TournamentDetails {
+  return new TournamentDetails({
+    tournamentId: mongoDocument._id,
+    tournamentName: TournamentName.from(mongoDocument.name),
+  });
+}
---
+++ backend/test/modules/tournament-details/core/application/TournamentDetailsWriteSide.spec.ts
@@ -0,0 +1,37 @@
+import { AddTournamentDetails } from '../../../../../src/modules/tournament-details/core/application/command/AddTournamentDetails';
+import { testTournamentsDetailsModule } from './TestTournamentDetailsModule';
+import { CommandResult } from '../../../../../src/shared/core/application/command/CommandResult';
+import Failure = CommandResult.Failure;
+
+describe('Tournament Details | Write Side', () => {
+  it('given not existing tournament details, when add tournament details, then tournament details were added', async () => {
+    //Given
+    const currentTime = new Date();
+    const tournamentsDetails = testTournamentsDetailsModule(currentTime);
+    const tournamentId = 'TournamentId';
+    const tournamentName = 'WrocLove';
+
+    //When
+    const addTournamentDetails = new AddTournamentDetails({ tournamentId, tournamentName });
+    const commandResult = await tournamentsDetails.executeCommand(addTournamentDetails);
+
+    //Then
+    expect(commandResult.isSuccess()).toBeTruthy();
+  });
+
+  it('given not existing tournament details, when add tournament details with tournament name shorter than 3 characters, then throw error', async () => {
+    //Given
+    const currentTime = new Date();
+    const tournamentsDetails = testTournamentsDetailsModule(currentTime);
+    const tournamentId = 'TournamentId';
+    const tournamentName = 'BB';
+
+    //When
+    const addTournamentDetails = new AddTournamentDetails({ tournamentId, tournamentName });
+    const commandResult = await tournamentsDetails.executeCommand(addTournamentDetails);
+
+    //Then
+    expect(commandResult.isSuccess()).toBeFalsy();
+    expect((commandResult as Failure).reason).toStrictEqual(new Error('Tournament name must have at least 3 characters.'));
+  });
+});
