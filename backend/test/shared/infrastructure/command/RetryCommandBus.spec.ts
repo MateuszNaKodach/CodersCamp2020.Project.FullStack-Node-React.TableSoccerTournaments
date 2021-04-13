de31062bc6b45a46e541f8ba20bcb36a4133376c
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-04-13 17:45:42 +0200

    TournamentsQueue | Add retry command bus

---
+++ backend/test/shared/infrastructure/command/RetryCommandBus.spec.ts
@@ -0,0 +1,21 @@
+import {CommandResult} from "../../../../src/shared/core/application/command/CommandResult";
+import {RetryCommandBus} from "../../../../src/shared/infrastructure/core/application/command/RetryCommandBus";
+import {StartTournament} from "./CommandsTestFixtures";
+
+describe('RetryCommandBus', function () {
+
+  //FIXME: Why mock do not show executes!?
+  xit('retrying max number of retires', () => {
+    const alwaysFailCommandBus = {
+      execute: jest.fn().mockImplementation(() => CommandResult.failureDueTo(new Error("Mock error"))),
+      registerHandler: jest.fn()
+    }
+    const commandBus = new RetryCommandBus(alwaysFailCommandBus, 3);
+
+    const startTournament = new StartTournament({ tournamentId: 'SampleId' });
+    commandBus.execute(startTournament)
+
+    expect(alwaysFailCommandBus.execute).toBeCalledTimes(3)
+  })
+
+});
---
+++ backend/test/test-support/shared/core/CommandBusMock.ts
@@ -0,0 +1,19 @@
+import {CommandBus, CommandPublisher} from '../../../../src/shared/core/application/command/CommandBus';
+import {CommandResult} from '../../../../src/shared/core/application/command/CommandResult';
+import {Command} from '../../../../src/shared/core/application/command/Command';
+import {HasConstructor} from "../../../../src/common/HasConstructor";
+import {CommandHandler} from "../../../../src/shared/core/application/command/CommandHandler";
+
+export function CommandBusMock(
+    alwaysReturn: CommandResult = CommandResult.success(),
+): CommandBus & { executeCalls: jest.Mock } {
+  const executeCalls: jest.Mock = jest.fn();
+  return {
+    async execute<CommandType extends Command>(command: CommandType): Promise<CommandResult> {
+      executeCalls(command);
+      return alwaysReturn;
+    }, registerHandler<CommandType>(commandType: HasConstructor<CommandType>, handler: CommandHandler<CommandType>): void {
+    },
+    executeCalls
+  };
+}
