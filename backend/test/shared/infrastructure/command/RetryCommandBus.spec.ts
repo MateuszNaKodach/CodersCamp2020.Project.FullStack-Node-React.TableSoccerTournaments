7fabaf47289c8026329d3717d3aabaf0621b0008
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-04-14 10:41:55 +0200

    RetryCommandBus | do not retries on success

--- backend/test/shared/infrastructure/command/RetryCommandBus.spec.ts
+++ backend/test/shared/infrastructure/command/RetryCommandBus.spec.ts
@@ -1,31 +1,44 @@
 import { CommandResult } from '../../../../src/shared/core/application/command/CommandResult';
 import { RetryCommandBus } from '../../../../src/shared/infrastructure/core/application/command/RetryCommandBus';
 import { StartTournament } from './CommandsTestFixtures';
+import {CommandBusMock} from "../../../test-support/shared/core/CommandBusMock";
+import Failure = CommandResult.Failure;
+import Success = CommandResult.Success;
 
 describe('RetryCommandBus', function () {
-  it('retrying max number of retires', async () => {
-    const alwaysFailCommandBus = {
-      execute: jest.fn().mockImplementation(() => CommandResult.failureDueTo(new Error('Mock error'))),
-      registerHandler: jest.fn(),
-    };
+  it('when every command fails, then should retried max number of retires', async () => {
+    const alwaysFailCommandBus = CommandBusMock(CommandResult.failureDueTo(new Error('Mock error')));
     const commandBus = new RetryCommandBus(alwaysFailCommandBus, 3);
 
     const startTournament = new StartTournament({ tournamentId: 'SampleId' });
-    await commandBus.execute(startTournament);
+    const result = await commandBus.execute(startTournament);
 
-    expect(alwaysFailCommandBus.execute).toBeCalledTimes(4);
+    expect(alwaysFailCommandBus.executeCalls).toBeCalledTimes(4);
+    expect(result.isSuccess()).toBeFalse();
+    expect((result as Failure).reason).toStrictEqual(new Error("Mock error"));
+  });
+
+  it('when every command succeed, then should not execute retries', async () => {
+    const alwaysSucceedCommandBus = CommandBusMock(CommandResult.success("SuccessValue"));
+    const commandBus = new RetryCommandBus(alwaysSucceedCommandBus, 100);
+
+    const startTournament = new StartTournament({ tournamentId: 'SampleId' });
+    const result = await commandBus.execute(startTournament);
+
+    expect(alwaysSucceedCommandBus.executeCalls).toBeCalledTimes(1);
+    expect(result.isSuccess()).toBeTrue();
+    expect((result as Success).value).toStrictEqual("SuccessValue");
   });
 
   it('when no retires, then execute command only once', async () => {
-    const alwaysFailCommandBus = {
-      execute: jest.fn().mockImplementation(() => CommandResult.failureDueTo(new Error('Mock error'))),
-      registerHandler: jest.fn(),
-    };
+    const alwaysFailCommandBus = CommandBusMock(CommandResult.failureDueTo(new Error('Mock error')));
     const commandBus = new RetryCommandBus(alwaysFailCommandBus, 0);
 
     const startTournament = new StartTournament({ tournamentId: 'SampleId' });
-    await commandBus.execute(startTournament);
+    const result = await commandBus.execute(startTournament);
 
-    expect(alwaysFailCommandBus.execute).toBeCalledTimes(1);
+    expect(alwaysFailCommandBus.executeCalls).toBeCalledTimes(1);
+    expect(result.isSuccess()).toBeFalse();
+    expect((result as Failure).reason).toStrictEqual(new Error("Mock error"));
   });
 });
