7be584a1521693145b2203790db9315a2bdd53ae
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-04-15 15:44:14 +0200

    Backend | Add saving TournamentPlace to MongoDoublesTournamentRepository

--- backend/src/modules/doubles-tournament/infrastructure/repository/mongo/MongoDoublesTournamentRepository.ts
+++ backend/src/modules/doubles-tournament/infrastructure/repository/mongo/MongoDoublesTournamentRepository.ts
@@ -1,9 +1,10 @@
-import { DoublesTournamentRepository } from '../../../core/application/DoublesTournamentRepository';
-import { DoublesTournament } from '../../../core/domain/DoublesTournament';
-import mongoose, { Schema } from 'mongoose';
-import { TeamId } from '../../../core/domain/TeamId';
-import { TournamentTeam } from '../../../core/domain/TournamentTeam';
-import { TournamentStatus } from '../../../core/domain/TournamentStatus';
+import {DoublesTournamentRepository} from '../../../core/application/DoublesTournamentRepository';
+import {DoublesTournament} from '../../../core/domain/DoublesTournament';
+import mongoose, {Schema} from 'mongoose';
+import {TeamId} from '../../../core/domain/TeamId';
+import {TournamentTeam} from '../../../core/domain/TournamentTeam';
+import {TournamentStatus} from '../../../core/domain/TournamentStatus';
+import {TournamentPlace} from "../../../core/domain/TournamentPlace";
 
 export class MongoDoublesTournamentRepository implements DoublesTournamentRepository {
   async findByTournamentId(tournamentId: string): Promise<DoublesTournament | undefined> {
@@ -13,17 +14,18 @@ export class MongoDoublesTournamentRepository implements DoublesTournamentReposi
 
   async save(doublesTournament: DoublesTournament): Promise<void> {
     await MongoDoublesTournament.findByIdAndUpdate(
-      { _id: doublesTournament.tournamentId },
-      {
-        _id: doublesTournament.tournamentId,
-        tournamentTeams: doublesTournament.tournamentTeams.map((team) => ({
-          teamId: team.teamId.raw,
-          firstTeamPlayer: team.firstTeamPlayer,
-          secondTeamPlayer: team.secondTeamPlayer,
-        })),
-        status: doublesTournament.status,
-      },
-      { upsert: true, useFindAndModify: true },
+        {_id: doublesTournament.tournamentId},
+        {
+          _id: doublesTournament.tournamentId,
+          tournamentTeams: doublesTournament.tournamentTeams.map((team) => ({
+            teamId: team.teamId.raw,
+            firstTeamPlayer: team.firstTeamPlayer,
+            secondTeamPlayer: team.secondTeamPlayer,
+          })),
+          status: doublesTournament.status,
+          places: doublesTournament.places.map(place => ({placeNumber: place.placeNumber, teamId: place.teamId.raw}))
+        },
+        {upsert: true, useFindAndModify: true},
     );
   }
 
@@ -37,12 +39,14 @@ type MongoDoublesTournament = {
   readonly _id: string;
   readonly tournamentTeams: { teamId: string; firstTeamPlayer: string; secondTeamPlayer: string }[];
   readonly status: string;
+  readonly places: { placeNumber: number, teamId: string; }[];
 } & mongoose.Document;
 
 const DoublesTournamentSchema = new mongoose.Schema({
   _id: Schema.Types.String,
-  tournamentTeams: [{ teamId: String, firstTeamPlayer: String, secondTeamPlayer: String }],
+  tournamentTeams: [{teamId: Schema.Types.String, firstTeamPlayer: Schema.Types.String, secondTeamPlayer: Schema.Types.String}],
   status: Schema.Types.String,
+  places: [{placeNumber: Schema.Types.Number, teamId: Schema.Types.String}]
 });
 
 const MongoDoublesTournament = mongoose.model<MongoDoublesTournament>('DoublesTournament', DoublesTournamentSchema);
@@ -52,14 +56,15 @@ function mongoDocumentToDomain(mongoDocument: MongoDoublesTournament): DoublesTo
     tournamentId: mongoDocument._id,
     tournamentTeams: [
       ...mongoDocument.tournamentTeams.map(
-        (team) =>
-          new TournamentTeam({
-            teamId: TeamId.from(team.teamId),
-            firstTeamPlayer: team.firstTeamPlayer,
-            secondTeamPlayer: team.secondTeamPlayer,
-          }),
+          (team) =>
+              new TournamentTeam({
+                teamId: TeamId.from(team.teamId),
+                firstTeamPlayer: team.firstTeamPlayer,
+                secondTeamPlayer: team.secondTeamPlayer,
+              }),
       ),
     ],
     status: mongoDocument.status as TournamentStatus,
+    places: mongoDocument.places.map(place => new TournamentPlace(place.placeNumber, TeamId.from(place.teamId)))
   });
 }
