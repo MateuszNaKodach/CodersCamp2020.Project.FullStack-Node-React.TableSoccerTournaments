437c4ddc63fb4c9166cd5f80d4be188bd00fe858
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-04-15 15:32:33 +0200

    Backend | Add Endpoint for single double tournaments

--- backend/src/modules/doubles-tournament/presentation/rest-api/DoublesTournamentRouter.ts
+++ backend/src/modules/doubles-tournament/presentation/rest-api/DoublesTournamentRouter.ts
@@ -1,47 +1,47 @@
-import { CommandPublisher } from '../../../../shared/core/application/command/CommandBus';
-import { DomainEventPublisher } from '../../../../shared/core/application/event/DomainEventBus';
-import { QueryPublisher } from '../../../../shared/core/application/query/QueryBus';
-import express, { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import { FindDoublesTournamentById, FindDoublesTournamentByIdResult } from '../../core/application/query/FindDoublesTournamentById';
-import { DoublesTournament } from '../../core/domain/DoublesTournament';
-import { TournamentTeamDto } from './response/TournamentTeamDto';
-import { TournamentTeamListDto } from './response/TournamentTeamListDto';
-import { MatchesQueueDto } from './response/MatchesQueueDto';
+import {CommandPublisher} from '../../../../shared/core/application/command/CommandBus';
+import {DomainEventPublisher} from '../../../../shared/core/application/event/DomainEventBus';
+import {QueryPublisher} from '../../../../shared/core/application/query/QueryBus';
+import express, {Request, Response} from 'express';
+import {StatusCodes} from 'http-status-codes';
+import {FindDoublesTournamentById, FindDoublesTournamentByIdResult} from '../../core/application/query/FindDoublesTournamentById';
+import {DoublesTournament} from '../../core/domain/DoublesTournament';
+import {TournamentTeamDto} from './response/TournamentTeamDto';
+import {TournamentTeamListDto} from './response/TournamentTeamListDto';
+import {MatchesQueueDto} from './response/MatchesQueueDto';
 import {
   FindMatchesQueueByTournamentId,
   FindMatchesQueueByTournamentIdResult,
 } from '../../core/application/query/FindMatchesQueueByTournamentId';
-import { QueuedMatchDto } from './response/QueuedMatchDto';
-import { FindAllDoublesTournaments, FindAllDoublesTournamentsResult } from '../../core/application/query/FindAllDoublesTournaments';
-import { TournamentListDto } from './response/TournamentListDto';
-import { TournamentDto } from './response/TournamentDto';
-import { StartTournament } from '../../core/application/command/StartTournament';
+import {QueuedMatchDto} from './response/QueuedMatchDto';
+import {FindAllDoublesTournaments, FindAllDoublesTournamentsResult} from '../../core/application/query/FindAllDoublesTournaments';
+import {TournamentListDto} from './response/TournamentListDto';
+import {TournamentDto} from './response/TournamentDto';
+import {StartTournament} from '../../core/application/command/StartTournament';
 import {TournamentPlaceDto, TournamentPlaceListDto} from "./response/TournamentPlacesListDto";
 
 export function doublesTournamentRouter(
-  commandPublisher: CommandPublisher,
-  eventPublisher: DomainEventPublisher,
-  queryPublisher: QueryPublisher,
+    commandPublisher: CommandPublisher,
+    eventPublisher: DomainEventPublisher,
+    queryPublisher: QueryPublisher,
 ): express.Router {
   const getTournamentTeamsByTournamentId = async (request: Request, response: Response) => {
-    const { tournamentId } = request.params;
-    const queryResult = await queryPublisher.execute<FindDoublesTournamentByIdResult>(new FindDoublesTournamentById({ tournamentId }));
+    const {tournamentId} = request.params;
+    const queryResult = await queryPublisher.execute<FindDoublesTournamentByIdResult>(new FindDoublesTournamentById({tournamentId}));
     if (!queryResult) {
-      return response.status(StatusCodes.NOT_FOUND).json({ message: `Doubles tournament with id = ${tournamentId} not found!` });
+      return response.status(StatusCodes.NOT_FOUND).json({message: `Doubles tournament with id = ${tournamentId} not found!`});
     }
     return response.status(StatusCodes.OK).json(new TournamentTeamListDto(toTournamentTeamDto(queryResult)));
   };
 
   const getMatchesQueueByTournamentId = async (request: Request, response: Response) => {
-    const { tournamentId } = request.params;
+    const {tournamentId} = request.params;
     const queryResult = await queryPublisher.execute<FindMatchesQueueByTournamentIdResult>(
-      new FindMatchesQueueByTournamentId({ tournamentId }),
+        new FindMatchesQueueByTournamentId({tournamentId}),
     );
     if (!queryResult) {
       return response
-        .status(StatusCodes.NOT_FOUND)
-        .json({ message: `Such Matches queue doesn't exist because doubles tournament with id = ${tournamentId} is not found!` });
+          .status(StatusCodes.NOT_FOUND)
+          .json({message: `Such Matches queue doesn't exist because doubles tournament with id = ${tournamentId} is not found!`});
     }
     const queuedMatchesDto: QueuedMatchDto[] = queryResult.queuedMatches.map((match) => ({
       matchNumber: match.matchNumber.raw,
@@ -58,27 +58,36 @@ export function doublesTournamentRouter(
     return response.status(StatusCodes.OK).json(new TournamentListDto(queryResult.map(toTournamentDto)));
   };
 
+  const getTournamentByTournamentId = async (request: Request, response: Response) => {
+    const {tournamentId} = request.params;
+    const queryResult = await queryPublisher.execute<FindDoublesTournamentByIdResult>(new FindDoublesTournamentById({tournamentId}));
+    if (!queryResult) {
+      return response.status(StatusCodes.NOT_FOUND).json({message: `Doubles tournament with id = ${tournamentId} not found!`});
+    }
+    return response.status(StatusCodes.OK).json(toTournamentDto(queryResult));
+  };
+
   const startTournament = async (request: Request, response: Response) => {
-    const { tournamentId } = request.params;
-    const commandResult = await commandPublisher.execute(new StartTournament({ tournamentId }));
+    const {tournamentId} = request.params;
+    const commandResult = await commandPublisher.execute(new StartTournament({tournamentId}));
     return commandResult.process(
-      () => response.status(StatusCodes.ACCEPTED).json({ message: 'Tournament was started.' }).send(),
-      (failureReason) => response.status(StatusCodes.BAD_REQUEST).json({ message: failureReason.message }),
+        () => response.status(StatusCodes.ACCEPTED).json({message: 'Tournament was started.'}).send(),
+        (failureReason) => response.status(StatusCodes.BAD_REQUEST).json({message: failureReason.message}),
     );
   };
 
   const getTournamentPlacesByTournamentId = async (request: Request, response: Response) => {
-    const { tournamentId } = request.params;
-    const queryResult = await queryPublisher.execute<FindDoublesTournamentByIdResult>(new FindDoublesTournamentById({ tournamentId }));
+    const {tournamentId} = request.params;
+    const queryResult = await queryPublisher.execute<FindDoublesTournamentByIdResult>(new FindDoublesTournamentById({tournamentId}));
     if (!queryResult) {
-      return response.status(StatusCodes.NOT_FOUND).json({ message: `Doubles tournament with id = ${tournamentId} not found!` });
+      return response.status(StatusCodes.NOT_FOUND).json({message: `Doubles tournament with id = ${tournamentId} not found!`});
     }
     return response.status(StatusCodes.OK).json(new TournamentPlaceListDto(toTournamentPlaceDto(queryResult)));
   };
 
   const router = express.Router();
   router.get('', getAllTournaments);
-  router.get('/:tournamentId', getAllTournaments);
+  router.get('/:tournamentId', getTournamentByTournamentId);
   router.post('/:tournamentId/start', startTournament);
   router.get('/:tournamentId/teams', getTournamentTeamsByTournamentId);
   router.get('/:tournamentId/matches', getMatchesQueueByTournamentId);
