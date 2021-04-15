19204ab3e5dac8d6ef91da40f087d874a51b803d
Pawel Szambelan <p.szambelan1@gmail.com>
2021-04-15 13:17:04 +0200

    #124 creating tournament

--- frontend/src/components/pages/TournamentsList/TournamentsList.tsx
+++ frontend/src/components/pages/TournamentsList/TournamentsList.tsx
@@ -1,23 +1,33 @@
 import CardTitleWithNavButton from "../../molecules/CardTitleWithNavButton/CardTitleWithNavButton";
-import {VerticalSpace} from "../../atoms/Shared/VerticalSpace";
+import { VerticalSpace } from "../../atoms/Shared/VerticalSpace";
 import TournamentCardsGallery from "../../organisms/TournamentsCardsGallery/TournamentsCardsGallery";
 import NewTournamentButton from "../../molecules/NewTournamentButton/NewTournamentButton";
-import {styled} from "@material-ui/core";
+import { styled } from "@material-ui/core";
+import { CreateTournamentForm } from "../../organisms/CreateTournamentForm/CreateTournamentForm";
+import React, { useState } from "react";
 
 const StyledWrapper = styled("div")({
-    position: 'relative'
-})
+  position: "relative",
+});
 
 const TournamentsList = () => {
-    return (
-        <StyledWrapper>
-            <CardTitleWithNavButton title={"Turnieje"}/>
-            <VerticalSpace height={30}/>
-            <TournamentCardsGallery/>
-            <VerticalSpace height={30}/>
-            <NewTournamentButton onLink={''}/>
-        </StyledWrapper>
-    )
-}
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [reloadData, setReloadData] = useState(false);
+
+  return (
+    <StyledWrapper>
+      <CardTitleWithNavButton title={"Turnieje"} />
+      <VerticalSpace height={30} />
+      <TournamentCardsGallery shouldReload={reloadData} />
+      <VerticalSpace height={30} />
+      <NewTournamentButton openForm={setOpenDrawer} />
+      <CreateTournamentForm
+        openDrawer={openDrawer}
+        returnToPrevState={setOpenDrawer}
+        reloadData={() => setReloadData(!reloadData)}
+      />
+    </StyledWrapper>
+  );
+};
 
 export default TournamentsList;
--- frontend/src/restapi/tournament-details/TournamentDetailsRestApi.ts
+++ frontend/src/restapi/tournament-details/TournamentDetailsRestApi.ts
@@ -1,32 +1,41 @@
 import axios from "axios";
-import {TournamentDetailsDto} from "./TournamentDetailsDto";
+import {
+  TournamentDetailsDto,
+  TournamentDetailsListDto,
+} from "./TournamentDetailsDto";
 
 export type TournamentDetailsRestApiConfig = {
-    readonly baseUrl: string;
+  readonly baseUrl: string;
 };
 
 const defaultConfig: TournamentDetailsRestApiConfig = {
-    baseUrl: "http://localhost:5000/rest-api",
+  baseUrl: "http://localhost:5000/rest-api",
 };
 
 export const TournamentDetailsRestApi = (
-    config?: Partial<TournamentDetailsRestApiConfig>
+  config?: Partial<TournamentDetailsRestApiConfig>
 ) => {
-    const currentConfig = {
-        ...defaultConfig,
-        config,
-        baseUrl:
-            process.env.REACT_APP_REST_API_BASE_URL ??
-            config?.baseUrl ??
-            defaultConfig.baseUrl,
-    };
-    return {
-        getAllTournamentsDetails(): Promise<TournamentDetailsDto[]> {
-            return axios
-                .get<TournamentDetailsDto[]>(
-                    `${currentConfig.baseUrl}/tournament-details/`
-                )
-                .then(res => res.data);
-        }
-    };
+  const currentConfig = {
+    ...defaultConfig,
+    config,
+    baseUrl:
+      process.env.REACT_APP_REST_API_BASE_URL ??
+      config?.baseUrl ??
+      defaultConfig.baseUrl,
+  };
+  return {
+    getAllTournamentsDetails(): Promise<TournamentDetailsListDto> {
+      return axios
+        .get<TournamentDetailsListDto>(
+          `${currentConfig.baseUrl}/tournament-details/`
+        )
+        .then((res) => res.data);
+    },
+    async addTournamentName(body: {
+      tournamentId: string;
+      tournamentName: string;
+    }): Promise<void> {
+      await axios.post(`${currentConfig.baseUrl}/tournament-details/`, body);
+    },
+  };
 };
