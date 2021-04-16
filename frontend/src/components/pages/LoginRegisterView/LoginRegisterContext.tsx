fe74845fc5c6d28c01c9cf3654dac8556d66731a
PiotrWR <pwrynio@gmail.com>
2021-04-16 16:49:46 +0200

    Add LoginRegister Context

---
+++ frontend/src/components/pages/LoginRegisterView/LoginRegisterContext.tsx
@@ -0,0 +1,8 @@
+import {createContext} from 'react';
+
+export const defaultObject = {
+   isUserLogged: false,
+   toggleLoggedState: ()=>{},
+}
+
+export  const LoginRegisterContext = createContext(defaultObject);
--- frontend/src/components/pages/LoginRegisterView/LoginRegisterView.tsx
+++ frontend/src/components/pages/LoginRegisterView/LoginRegisterView.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 import {StyledCard} from "../../atoms/StyledCard/StyledCard";
 import LoginRegisterLabelTabs from "./LoginRegisterLabelTabs";
 
-
 export default function IconLabelTabs() {
-
 
    return (
       <>
          <LoginRegisterLabelTabs/>
          <StyledCard >
+
          </StyledCard>
       </>
    );
