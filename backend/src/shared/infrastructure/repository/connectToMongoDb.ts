3144080dcd9f90ecf385609abaef4900ad0336e5
Mateusz Nowak <mateusz.nowak@vonage.com>
2021-04-22 19:39:57 +0200

    Fix format

--- backend/src/shared/infrastructure/repository/connectToMongoDb.ts
+++ backend/src/shared/infrastructure/repository/connectToMongoDb.ts
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
 export async function connectToMongoDb(): Promise<void> {
-    const connectionString = process.env.MONGO_CONNECTION_STRING;
-    if (!connectionString) {
-        throw new Error("Connection string to MongoDB is not defined!");
-    }
-    await mongoose
-        .connect(connectionString, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        })
-        .then(() => console.log(`[TableSoccerTournamentsApplication]: Application connected to mongoDB instance at: ${connectionString}`))
-        .catch((error) =>
-            console.error(`[TableSoccerTournamentsApplication]: Error while connecting to mongo db at: ${connectionString}`, error),
-        );
+  const connectionString = process.env.MONGO_CONNECTION_STRING;
+  if (!connectionString) {
+    throw new Error('Connection string to MongoDB is not defined!');
+  }
+  await mongoose
+    .connect(connectionString, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => console.log(`[TableSoccerTournamentsApplication]: Application connected to mongoDB instance at: ${connectionString}`))
+    .catch((error) =>
+      console.error(`[TableSoccerTournamentsApplication]: Error while connecting to mongo db at: ${connectionString}`, error),
+    );
 }
