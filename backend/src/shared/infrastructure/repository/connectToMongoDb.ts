702edb99f5418b20128c10c90bace10b7cb0a0a7
Tomek <tomasz.dworniczak@hotmail.com>
2021-04-22 16:09:37 +0200

    Code Format

--- backend/src/shared/infrastructure/repository/connectToMongoDb.ts
+++ backend/src/shared/infrastructure/repository/connectToMongoDb.ts
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
 export async function connectToMongoDb(): Promise<void> {
-    const connectionString = `mongodb+srv://${process.env.MONGO_USER_HEROKU}:${process.env.MONGO_PASSWORD_HEROKU}@${process.env.MONGO_HOST_HEROKU}/${process.env.MONGO_DB_HEROKU}?retryWrites=true&w=majority`;
-    await mongoose
-        .connect(connectionString, {
-            user: process.env.MONGO_USER_HEROKU,
-            pass: process.env.MONGO_PASSWORD_HEROKU,
-            dbName: process.env.MONGO_DB_HEROKU,
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        })
-        .then(() => console.log(`[TableSoccerTournamentsApplication]: Application connected to mongoDB instance at: ${connectionString}`))
-        .catch((error) =>
-            console.error(`[TableSoccerTournamentsApplication]: Error while connecting to mongo db at: ${connectionString}`, error),
-        );
+  const connectionString = `mongodb+srv://${process.env.MONGO_USER_HEROKU}:${process.env.MONGO_PASSWORD_HEROKU}@${process.env.MONGO_HOST_HEROKU}/${process.env.MONGO_DB_HEROKU}?retryWrites=true&w=majority`;
+  await mongoose
+    .connect(connectionString, {
+      user: process.env.MONGO_USER_HEROKU,
+      pass: process.env.MONGO_PASSWORD_HEROKU,
+      dbName: process.env.MONGO_DB_HEROKU,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
+    .then(() => console.log(`[TableSoccerTournamentsApplication]: Application connected to mongoDB instance at: ${connectionString}`))
+    .catch((error) =>
+      console.error(`[TableSoccerTournamentsApplication]: Error while connecting to mongo db at: ${connectionString}`, error),
+    );
 }
